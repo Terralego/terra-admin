c37b190e2978b5e984bd230dc3eeda809f1aa4db
Hadrien Lanneau <hadrien@lanneau.me>
2018-12-19 17:01:44 +0100

    Configure i18n

---
+++ src/config/i18n.js
@@ -0,0 +1,30 @@
+import i18n from 'i18next';
+import XHR from 'i18next-xhr-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { reactI18nextModule } from 'react-i18next';
+
+i18n
+  .use(XHR)
+  .use(LanguageDetector)
+  .use(reactI18nextModule) // if not using I18nextProvider
+  .init({
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    },
+    fallbackLng: 'en',
+    debug: true,
+
+    interpolation: {
+      escapeValue: false, // not needed for react!!
+    },
+
+    // react i18next special options (optional)
+    react: {
+      wait: false,
+      bindI18n: 'languageChanged loaded',
+      bindStore: 'added removed',
+      nsMode: 'default',
+    },
+  });
+
+export default i18n;

58bbf874c9f278f177e4880de9c58190b8e36acd
Aline <chetta.aline@gmail.com>
2019-02-05 14:25:59 +0100

    Move inputMap into components

---
+++ src/modules/OPP/components/InputMap/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import mapboxgl from 'mc-tf-test/node_modules/mapbox-gl';
+import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css';
+import { Map } from 'mc-tf-test';
+
+export class InputMap extends React.Component {
+  static propTypes = {
+    configMap: PropTypes.shape({
+      accessToken: PropTypes.string,
+      backgroundStyle: PropTypes.string,
+      center: PropTypes.arrayOf(PropTypes.number),
+      maxBounds: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.array), PropTypes.bool]),
+    }),
+    value: PropTypes.arrayOf(PropTypes.number),
+    onChange: PropTypes.func,
+  };
+
+  static defaultProps = {
+    configMap: {},
+    value: [],
+    onChange () {
+    },
+  };
+
+  state = {
+    marker: null,
+  };
+
+  componentDidUpdate () {
+    const { marker } = this.state;
+    if (marker) {
+      const { value } = this.props;
+      marker.setLngLat(value);
+    }
+  }
+
+  initMap = map => {
+    this.setState({ marker: new mapboxgl.Marker() });
+
+    const { marker } = this.state;
+    const { onChange } = this.props;
+
+    marker.addTo(map);
+
+    map.on('click', e => {
+      onChange([e.lngLat.lng, e.lngLat.lat]);
+      marker.setLngLat(e.lngLat);
+    });
+  };
+
+  render () {
+    const { configMap } = this.props;
+    return (
+      <div className="input-map">
+        <Map
+          {...configMap}
+          onMapLoaded={this.initMap}
+        />
+      </div>
+    );
+  }
+}
+
+export default (InputMap);

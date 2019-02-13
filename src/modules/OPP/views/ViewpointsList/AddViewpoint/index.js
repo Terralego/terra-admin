66602e6989797627afb30f7372012af471266ef0
Aline <chetta.aline@gmail.com>
2019-02-13 15:10:11 +0100

    Coverage Card with Link
    Terralego/Customers/Oppm/oppm-front#36

--- src/modules/OPP/views/ViewpointsList/AddViewpoint/index.js
+++ src/modules/OPP/views/ViewpointsList/AddViewpoint/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Card, Classes } from '@blueprintjs/core';
+import { Card } from '@blueprintjs/core';
 import { withNamespaces } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
 export const AddViewpoint = ({ t }) => (
-  <Card interactive className={Classes.DARK}>
-    <Link to="viewpoints/create">{t('common.add')}</Link>
-  </Card>
+  <Link className="card-link--add" to="viewpoints/create">
+    <Card interactive className="item--add">
+      <p>{t('common.add')}</p>
+    </Card>
+  </Link>
 );
 
 export default withNamespaces()(AddViewpoint);

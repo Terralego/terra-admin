2cd69ee511809b308fd5180c99adf1a8a979a272
Aline <chetta.aline@gmail.com>
2019-04-12 17:53:47 +0200

    Add navigation sidebar

--- src/modules/OPP/Nav.js
+++ src/modules/OPP/Nav.js
@@ -1,12 +1,52 @@
 import React from 'react';
+
+import {
+  Tooltip,
+  Position,
+  Button,
+} from '@blueprintjs/core';
 import { NavLink } from 'react-router-dom';
 import { withNamespaces } from 'react-i18next';
+
+import iconPoi from './images/icon_poi.svg';
+import iconCampaign from './images/no_photo.png';
 
 import './nav.scss';
 
 export const Nav = ({ t }) => (
-  <ul>
-    <li><NavLink to="/opp/viewpoints">{t('opp.nav.viewpoints')}</NavLink></li>
+  <ul className="navbar__mainMenu">
+    <li>
+      <Tooltip
+        content={t('opp.nav.viewpoints')}
+        position={Position.LEFT}
+        usePortal={false}
+      >
+        <NavLink
+          exact
+          to="/opp/viewpoints"
+        >
+          <Button>
+            <img src={iconPoi} alt={t('opp.nav.viewpoints')} />
+          </Button>
+        </NavLink>
+      </Tooltip>
+    </li>
+    <li>
+      <Tooltip
+        content={t('opp.nav.compaign')}
+        position={Position.LEFT}
+        usePortal={false}
+      >
+        <NavLink
+          exact
+          to="/opp/compaign"
+        >
+          <Button>
+            <img src={iconPoi} alt={t('opp.nav.compaign')} />
+          </Button>
+        </NavLink>
+      </Tooltip>
+    </li>
   </ul>
 );
 

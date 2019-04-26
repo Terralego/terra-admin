8c94690c945d2ccd61204650c051947f4a82b87f
Hadrien Lanneau <hadrien@lanneau.me>
2019-04-26 09:18:01 +0200

    Users module bootstrap

---
+++ src/modules/Users/config.js
@@ -0,0 +1,9 @@
+export default {
+  title: 'users.project',
+  path: '/users',
+  permission: 'admin.users',
+  nav: [{
+    label: 'users.nav.list',
+    href: '',
+  }],
+};
---
+++ src/modules/Users/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import config from './config';
+
+import './styles.scss';
+
+// const { path } = config;
+
+export const Users = () => (
+  <p>Users</p>
+);
+
+Users.config = config;
+
+export default Users;

a6fa49be4dbff913925b478f239d8ecce748ab02
Hadrien Lanneau <hadrien@lanneau.me>
2019-04-26 10:22:25 +0200

    I18n management in ra

--- src/modules/Users/index.js
+++ src/modules/Users/index.js
@@ -3,17 +3,24 @@ import { Admin, Resource } from 'react-admin';
 
 import config from './config';
 
+import { withLocale } from '../../components/Locale';
 import dataProvider from './services/dataProvider';
 import authProvider from '../../services/react-admin/authProvider';
+import i18nProvider from '../../services/react-admin/i18nProvider';
 import { UserList, UserEdit, UserCreate } from './components/users';
 
 import './styles.scss';
 
-export const Users = () => (
-  <Admin dataProvider={dataProvider} authProvider={authProvider}>
+export const Users = withLocale(({ locale }) => (
+  <Admin
+    dataProvider={dataProvider}
+    authProvider={authProvider}
+    i18nProvider={i18nProvider}
+    locale={`${locale}`.substr(0, 2)}
+  >
     <Resource name="user" list={UserList} edit={UserEdit} create={UserCreate} />
   </Admin>
-);
+));
 
 Users.config = config;
 

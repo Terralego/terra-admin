30ce8f6b9582405836dce18abf6fcc8eec200a18
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-05-14 11:42:34 +0200

    Re-split Edit & Create components for dataLayer

--- src/modules/DataLayer/views/index.js
+++ src/modules/DataLayer/views/index.js
@@ -1,4 +1,5 @@
-import { DataLayerCreate as create, DataLayerEdit as edit } from './Form';
+import create from './Create';
+import edit from './Edit';
 import list from './List';
 
 export default { create, edit, list };

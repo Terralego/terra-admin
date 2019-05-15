f769efb204adb96a65a79fb1316558c5b8f87110
Florian Sommariva <florian.sommariva@makina-corpus.com>
2019-05-15 10:07:46 +0200

    Display logo, if not project name, if not main.title local in the banner
    ref #21

---
+++ src/views/Main/Header/Header.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import {
+  Navbar,
+  NavbarGroup,
+  NavbarHeading,
+  Classes,
+} from '@blueprintjs/core';
+
+import './header.scss';
+import UserDropdown from '../UserDropdown';
+
+const logo = ({ title, logo: { src = false, alt = title }, t }) => {
+  if (!src) {
+    if (!alt) {
+      return <span>{t('main.title')}</span>;
+    }
+    return <span>{alt}</span>;
+  }
+  return <img className="main-header__logo" src={src} alt={alt} />;
+};
+
+export const Header = ({ authenticated, ...props }) => (
+  <header className="main-header">
+    <Navbar className={Classes.DARK}>
+      <NavbarGroup align="left">
+        <NavbarHeading>
+          <NavLink to="/">
+            {logo(props)}
+          </NavLink>
+        </NavbarHeading>
+      </NavbarGroup>
+      {authenticated && (
+        <UserDropdown />
+      )}
+    </Navbar>
+  </header>
+);
+
+export default Header;

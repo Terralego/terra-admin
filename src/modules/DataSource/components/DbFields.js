faaa5dcb40d0b2f1524a044197c3bf4efcb3de35
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-05-15 15:21:31 +0200

    Fix field name & use numeric values for source refresh field

--- src/modules/DataSource/components/DbFields.js
+++ src/modules/DataSource/components/DbFields.js
@@ -12,13 +12,13 @@ const DbFields = () => (
     <LongTextInput source="query" type="text" />
     <TextInput source="geom_field" type="text" label="Geometry field name" />
     <SelectInput
-      source="refresh_rate"
+      source="refresh"
       choices={[
-        { id: 'never', name: 'Never update' },
-        { id: 'hourly', name: 'Hourly' },
-        { id: 'daily', name: 'Daily' },
-        { id: 'weekly', name: 'Weekly' },
-        { id: 'monthly', name: 'Monthly' },
+        { id: -1, name: 'Never update' },
+        { id: 60, name: 'Hourly' },
+        { id: (60 * 24), name: 'Daily' },
+        { id: (60 * 24 * 7), name: 'Weekly' },
+        { id: (60 * 24 * 30), name: 'Monthly' },
       ]}
     />
   </FieldGroup>

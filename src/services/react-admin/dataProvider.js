3a7363251d325dcaaca38444560abcf893314953
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-05-21 09:18:19 +0200

    Manage switching to multipart-form for geosource file upload

--- src/services/react-admin/dataProvider.js
+++ src/services/react-admin/dataProvider.js
@@ -1,12 +1,51 @@
 import drfProvider from 'ra-data-drf';
 import Api from '@terralego/core/modules/Api';
 
-export default (...args) => {
-  const [queryType, resource, params] = args;
+export default async (...args) => {
+  const [type, resource, params] = args;
 
   // Manage custom query type
-  if (queryType === 'REFRESH') {
-    return fetch(`${Api.host}/${resource}/${params.id}/refresh/`);
+  if (type === 'REFRESH') {
+    return Api.request(`${resource}/${params.id}/refresh/`);
+  }
+
+  // Manage file upload
+  if (['CREATE', 'UPDATE'].includes(type) && resource === 'geosource') {
+    const body = new FormData();
+
+    Object.keys(params.data).forEach(key => {
+      if (key === 'file') { return; }
+
+      const value = typeof params.data[key] === 'object'
+        ? JSON.stringify(params.data[key])
+        : params.data[key];
+
+      body.append(key, value);
+    });
+
+    if (params.data.file && params.data.file.rawFile) {
+      body.append('file', params.data.file.rawFile);
+    }
+
+    let response;
+
+    switch (type) {
+      case 'CREATE':
+        response = await Api.request('geosource/', { method: 'POST', body });
+        break;
+
+      case 'UPDATE':
+        response = await Api.request(`geosource/${params.id}/`, { method: 'PUT', body });
+        break;
+      default:
+    }
+
+    switch (type) {
+      case 'CREATE':
+        return { data: response, id: response.id };
+      default:
+        return { data: response };
+    }
   }
 
   return drfProvider(Api.host)(...args);

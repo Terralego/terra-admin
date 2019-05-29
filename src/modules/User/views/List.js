f46247a499df177dae28b62b94c463595e718814
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-05-29 14:44:41 +0200

    Add filter for active users

--- src/modules/User/views/List.js
+++ src/modules/User/views/List.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import {
   List,
+  Filter,
   Datagrid,
   TextField,
   EditButton,
   BooleanField,
+  NullableBooleanInput,
   Pagination,
 } from 'react-admin';
 import ArrayCountField from '../../../components/react-admin/ArrayCountField';
 import CommonBulkActionButtons from '../../../components/react-admin/CommonBulkActionButtons';
+
+const UserListFilters = props => (
+  <Filter {...props}>
+    <NullableBooleanInput source="is_active" label="user.form.active" />
+  </Filter>
+);
 
 const UserListPagination = props =>
   <Pagination rowsPerPageOptions={[]} {...props} />;
@@ -17,6 +25,7 @@ export const UserList = props => (
   <List
     {...props}
     bulkActionButtons={<CommonBulkActionButtons />}
+    filters={<UserListFilters />}
     perPage={100}
     pagination={<UserListPagination />}
   >

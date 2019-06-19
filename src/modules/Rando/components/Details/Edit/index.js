1b89a21664bd01fe743cec325c3a2a1255f0c44c
Florian Sommariva <florian.sommariva@makina-corpus.com>
2019-06-19 17:39:10 +0200

    Fix saveFeature action

--- src/modules/Rando/components/Details/Edit/index.js
+++ src/modules/Rando/components/Details/Edit/index.js
@@ -3,8 +3,18 @@ import { withNamespaces } from 'react-i18next';
 import { connectRandoProvider } from '../../../services/RandoProvider';
 import Edit from './Edit';
 
-export default withRouter(connectRandoProvider(
-  'map',
-  'feature',
-  'saveFeature',
-)(withNamespaces()(Edit)));
+export default withRouter(connectRandoProvider(({
+  layersList,
+  map,
+  feature,
+  saveFeature,
+}, {
+  match: { params: { layer, id } },
+}) => ({
+  map,
+  feature,
+  saveFeature,
+  layer: layersList.find(({ name }) => name === layer),
+  paramLayer: layer,
+  paramId: id,
+}))(withNamespaces()(Edit)));

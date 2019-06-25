97d379d5730ef59c873eef4f548c68c486726197
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-06-25 11:26:40 +0200

    Group user form fields in a single component

--- src/modules/User/views/Edit.js
+++ src/modules/User/views/Edit.js
@@ -1,27 +1,13 @@
 import React from 'react';
-import {
-  Edit, SimpleForm,
-  DisabledInput,
-  TextInput,
-  BooleanInput,
-} from 'react-admin';
+import { Edit } from 'react-admin';
 
-import JSONInput from '../../../components/react-admin/JSONInput';
-import TermListInput from '../../../components/react-admin/TermListInput';
+import UserFields from '../components/UserFields';
 
 const UserTitle = ({ record }) => <span>User {record ? `"${record.email}"` : ''}</span>;
 
 export const UserEdit = props => (
   <Edit title={<UserTitle />} undoable={false} {...props}>
-    <SimpleForm>
-      <DisabledInput source="id" label="user.form.id" />
-      <DisabledInput source="uuid" label="user.form.uuid" />
-      <TextInput source="email" type="email" label="user.form.email" />
-      <BooleanInput source="is_superuser" label="user.form.superuser" />
-      <BooleanInput source="is_active" label="user.form.active" />
-      <TermListInput source="permissions" label="user.form.permissions" />
-      <JSONInput source="properties" label="user.form.additional-information" fullWidth />
-    </SimpleForm>
+    <UserFields edit />
   </Edit>
 );
 

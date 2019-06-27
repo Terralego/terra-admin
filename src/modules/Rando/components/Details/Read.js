e6d685130601479896e12464adea836f20d27649
Florian Sommariva <florian.sommariva@makina-corpus.com>
2019-06-27 17:20:49 +0200

    Styles actions buttons for read/create/update feature
    ref #50

--- src/modules/Rando/components/Details/Read.js
+++ src/modules/Rando/components/Details/Read.js
@@ -21,29 +21,31 @@ const formattedProp = value => {
 const Read = ({
   t,
   match: { params: { layer, id } },
-  schema: { properties },
+  schema: { properties = {} },
 }) => {
-  if (!properties) return null;
   const { name: { default: title } = {} } = properties;
+  const hasProperties = !!Object.keys(properties).length;
 
   return (
     <div className="details">
       <div className="details__header">
         <h2 className="details__title">{title || t(NO_FEATURE)}</h2>
       </div>
-      <div className="details__content">
-        <h3 className="details__subtitle">{t('rando.details.informations')}</h3>
-        <ul className="details__list">
-          {Object.keys(properties).map(prop => (
-            <li key={prop} className="details__list-item">
-              <strong className="details__list-label">{properties[prop].title || prop}</strong>
-              <span className={classnames('details__list-value', { 'details__list-value--empty': !properties[prop].default })}>
-                {formattedProp(properties[prop].default || t(NO_FEATURE))}
-              </span>
-            </li>
-          ))}
-        </ul>
-      </div>
+      {hasProperties && (
+        <div className="details__content">
+          <h3 className="details__subtitle">{t('rando.details.informations')}</h3>
+          <ul className="details__list">
+            {Object.keys(properties).map(prop => (
+              <li key={prop} className="details__list-item">
+                <strong className="details__list-label">{properties[prop].title || prop}</strong>
+                <span className={classnames('details__list-value', { 'details__list-value--empty': !properties[prop].default })}>
+                  {formattedProp(properties[prop].default || t(NO_FEATURE))}
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
       <Actions paramId={id} paramLayer={layer} displayUpdate displayDelete />
     </div>
   );

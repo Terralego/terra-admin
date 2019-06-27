e3fd8244fc3b7e899d0f33f90f0a5610cd561b1a
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-06-27 15:43:21 +0200

    Add ability for base layout to display notifications

--- src/components/react-admin/Layout/RALayout.js
+++ src/components/react-admin/Layout/RALayout.js
@@ -1,3 +1,11 @@
-export const RALayout = ({ children }) => children;
+import React, { createElement } from 'react';
+import { Notification } from 'react-admin';
+
+export const RALayout = ({ children }) => (
+  <>
+    {createElement(Notification)}
+    {children}
+  </>
+);
 
 export default RALayout;

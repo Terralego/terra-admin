883a0cb940ed378fa3787cfee145dd2b0a59eed7
Jeremie Pardou <jpardou@makina-corpus.com>
2019-06-28 09:48:59 +0200

    Fix Terralego/Customers/Paca/paca-project#559 use default value

--- src/components/react-admin/JSONInput.js
+++ src/components/react-admin/JSONInput.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import {
-  FormDataConsumer,
-  REDUX_FORM_NAME,
-  withDataProvider,
   Labeled,
+  addField,
 } from 'react-admin';
 
 import { JsonEditor as Editor } from 'jsoneditor-react';
@@ -13,47 +11,42 @@ import ace from 'brace';
 import 'brace/mode/json';
 import 'brace/theme/github';
 
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { change } from 'redux-form';
-
-const sanitizeObject = data => {
-  if (typeof data === 'string') {
-    try {
-      return JSON.parse(data);
-    } catch (err) {
-      // eslint-disable-next-line no-console
-      console.error(err);
-    }
-  }
-
-  return data;
-};
-
 const sanitizeRestProps = ({
   basePath, i18n, i18nOptions, id, index,
   lng, resource, source, tReady, ...rest
 }) => rest;
 
-export const JSONInput = withDataProvider(({ dispatch, dataProvider, source, ...props }) => (
-  <Labeled {...props}>
-    <FormDataConsumer>
-      {({ defaultValue = {}, formData: { [source]: data = defaultValue } = {} }) => (
+
+export const JSONInput = addField(({ input, meta, source, defaultValue, ...props }) => {
+  const initialValue = input.value || defaultValue || {}; // TODO not working with empty string ...
+  const [value] = React.useState(initialValue);
+
+  const handleChange = newValue => {
+    // TODO We may add a debounce here to speed up process
+    meta.touched = true;
+    return input.onChange(newValue);
+  };
+
+  return (
+    <Labeled label={source} {...props}>
+      <>
+        <pre style={{ display: 'none' }}>debug: {JSON.stringify(input.value, null, 2)}</pre>
         <Editor
-          value={sanitizeObject(data)}
+          value={value}
           ace={ace}
           theme="ace/theme/github"
           mode="code"
-          allowedModes={['code', 'tree', 'view']}
+          allowedModes={['code', 'tree']}
           navigationBar={false}
           search={false}
-          name={source}
-          onChange={newData =>
-            dispatch(change(REDUX_FORM_NAME, source, newData, null, 2))}
+          onChange={val => handleChange(val)}
           {...sanitizeRestProps(props)}
         />
-      )}
-    </FormDataConsumer>
-  </Labeled>
-));
+        {meta.touched && meta.error && <p className="error">{meta.error}</p>}
+      </>
+    </Labeled>
+  );
+});
+
 
 export default JSONInput;
--- src/modules/DataLayer/components/StyleField.js
+++ src/modules/DataLayer/components/StyleField.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { addField } from 'react-admin';
 
 import { JSONInput } from '../../../components/react-admin/JSONInput';
 import withRandomColor from './withRandomColor';
@@ -31,26 +30,4 @@ const StyleField = withRandomColor(({ randomColor, sourceData: { geom_type: type
   />
 ));
 
-const parse = value => {
-  try {
-    return JSON.parse(value);
-  } catch (e) {
-    return value;
-  }
-};
-
-export default addField(StyleField, {
-  parse,
-  validate: value => {
-    if (!value || typeof value !== 'object') {
-      return 'invalid json';
-    }
-    if (!value.type) {
-      return 'type not found';
-    }
-    if (!value.paint) {
-      return 'paint not found';
-    }
-    return null;
-  },
-});
+export default StyleField;

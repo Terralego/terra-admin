62b2f1630aa68242d2ebb966b260e8879810733c
Hadrien Lanneau <hadrien@lanneau.me>
2019-07-22 16:30:31 +0200

    Display help to fill legends

---
+++ src/components/react-admin/HelpContent.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { translate } from 'react-admin';
+
+/* eslint-disable import/no-extraneous-dependencies */
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+/* eslint-enable */
+
+const HelpContent = ({ translate: t, title, content, children }) => (
+  <>
+    <Card>
+      <CardContent>
+        {title && <Typography variant="subheading" gutterBottom>{t(title)}</Typography>}
+        <Typography color="textSecondary" paragraph>
+          {children || <span dangerouslySetInnerHTML={{ __html: t(content) }} />}
+        </Typography>
+      </CardContent>
+    </Card>
+  </>
+);
+
+HelpContent.propTypes = {
+  title: PropTypes.string,
+  content: PropTypes.string,
+  children: PropTypes.element,
+};
+
+HelpContent.defaultProps = {
+  title: '',
+  content: '',
+  children: undefined,
+};
+
+export default translate(HelpContent);

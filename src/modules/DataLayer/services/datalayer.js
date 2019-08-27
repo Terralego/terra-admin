a968f8dd274d8430afb70d02e9e8f07fac6958cb
Jérémie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2019-08-27 17:55:41 +0200

    Simplify viewList processing code
    Co-Authored-By: mab <benjamin.marguin@makina-corpus.com>

--- src/modules/DataLayer/services/datalayer.js
+++ src/modules/DataLayer/services/datalayer.js
@@ -6,14 +6,11 @@ export const fetchViewList = async () => {
   if (viewList === null) {
     const apiViews =  await Api.request('geolayer/view/');
 
-    // Object tranformation from backend format
-    // To frontend one.
+    // Object transformation from backend format
+    // to frontend one.
     // Should be removed in a future release
     viewList = Object.entries(apiViews)
-      .map(elt => {
-        const [key, { pk, ...rest }] = elt;
-        return { ...rest, id: pk, key };
-      })
+      .map(([key, { pk: id, ...rest }]) => ({ ...rest, id, key }))
       .sort(({ id: idA }, { id: idB }) => idA - idB);
   }
 

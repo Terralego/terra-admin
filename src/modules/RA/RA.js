6d58e4ee93a5064ff85dc34d1d3f669e248e0774
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-09-11 23:21:35 +0200

    Remove useless provider proxy file

--- src/modules/RA/RA.js
+++ src/modules/RA/RA.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { Admin, Resource } from 'react-admin';
 
+import dataProvider from '../../services/react-admin/dataProvider';
+import enhanceDataProvider from '../../services/react-admin/enhanceDataProvider';
+import authProvider from '../../services/react-admin/authProvider';
+import i18nProvider from '../../services/react-admin/i18nProvider';
+
 import { withLocale } from '../../components/Locale';
-import providers from '../../services/react-admin/providers';
 import RALayout from '../../components/react-admin/Layout';
 
 import { resources } from './ra-modules';
@@ -13,7 +17,10 @@ export const CustomAdmin = ({ locale, history }) => (
     appLayout={RALayout}
     locale={`${locale}`.substr(0, 2)}
     history={history}
-    {...providers}
+
+    dataProvider={enhanceDataProvider(dataProvider)}
+    authProvider={authProvider}
+    i18nProvider={i18nProvider}
   >
     {resources.map(resource => <Resource key={resource.name} {...resource} />)}
   </Admin>

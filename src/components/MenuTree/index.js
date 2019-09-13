2d9a98838a8b9d7bab9dd5ab9ecffd3f5e59ade9
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-09-13 17:37:07 +0200

    Manage menu content with a more standard setup

---
+++ src/components/MenuTree/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import { withNamespaces } from 'react-i18next';
+import { MenuItem } from '@blueprintjs/core';
+
+export const MenuTree = ({ t, href, items = [], label }) => {
+  if (!items.length) {
+    if (!href) {
+      return <MenuItem text={t(label)} />;
+    }
+
+    return (
+      <NavLink key={href} to={href}>
+        <MenuItem tagName="span" text={t(label)} />
+      </NavLink>
+    );
+  }
+
+  return (
+    <MenuItem text={t(label)}>
+      {items.map(item => <MenuTree t={t} key={item.label} {...item} />)}
+    </MenuItem>
+  );
+};
+
+export default withNamespaces()(MenuTree);
--- src/views/Main/MenuDropdown/MenuDropdown.js
+++ src/views/Main/MenuDropdown/MenuDropdown.js
@@ -10,16 +10,28 @@ import {
   Position,
 } from '@blueprintjs/core';
 
+import MenuTree from '../../../components/MenuTree';
+
 export const MenuDropdown = ({ t, modules = [] }) => {
   const [{ config: { nav = [] } = {} } = {}] = modules;
   if (modules.length <= 1 && nav.length <= 1) {
     return null;
   }
+
+  const menuContent = modules.reduce((acc, { config: { menu = [] } = {} }) => [
+    ...acc,
+    ...menu,
+  ], []);
+
   return (
     <Popover
       content={(
         <Menu>
-          {modules.map(Component => (
+          {!!menuContent.length && (
+            menuContent.map(menuProps => <MenuTree key={menuProps.label} {...menuProps} />)
+          )}
+
+          {!menuContent.length && modules.map(Component => (
             <MenuItem
               key={Component.name}
               className={Classes.MINIMAL}

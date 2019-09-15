d51605bc0a1d81a63b4b325057b6ce8226ea2e6a
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-09-15 11:03:59 +0200

    Add tabs for viewpoint data

--- src/modules/RA/Viewpoint/components/ViewpointFields.js
+++ src/modules/RA/Viewpoint/components/ViewpointFields.js
@@ -1,18 +1,68 @@
 import React from 'react';
 
 import {
-  TextInput,
-  SimpleForm,
+  Datagrid,
+  DateField,
   DisabledInput,
+  FormTab,
+  LongTextInput,
+  ReferenceArrayField,
+  SelectInput,
+  TabbedForm,
+  TextField,
+  TextInput,
+  translate,
 } from 'react-admin';
+import RichTextInput from 'ra-input-rich-text';
+import { RES_PICTURE } from '../../ra-modules';
 
-const ViewpointFields = ({ edit = false, ...props }) => (
-  <SimpleForm {...props}>
-    {edit && <DisabledInput source="id" />}
-    <TextInput source="label" />
-    <TextInput source="voie" />
-    <TextInput source="geometry.coordinates" />
-  </SimpleForm>
+const ViewpointFields = ({ edit = false, translate: t, ...props }) => (
+  <TabbedForm {...props}>
+    <FormTab label={t('resources.viewpoint.tabs.general')}>
+      {edit && <DisabledInput source="id" />}
+      <TextInput source="label" />
+
+      <TextInput source="address" />
+      <SelectInput source="city_id" choices={[]} />
+
+      <TextInput source="geometry.coordinates" />
+    </FormTab>
+
+    <FormTab label={t('resources.viewpoint.tabs.repeat')} path="repeat">
+      <TextInput source="properties.altitude" />
+      <TextInput source="properties.hauteur" />
+      <TextInput source="properties.orientation" />
+      <TextInput source="properties.focale_35mm" />
+      <TextInput source="properties.focale_objectif" />
+      <SelectInput source="properties.frequency" choices={[]} />
+      <SelectInput source="properties.difficulty" choices={[]} />
+      <TextInput source="properties.note" />
+    </FormTab>
+
+    <FormTab label={t('resources.viewpoint.tabs.landscape')} path="landscape">
+      <RichTextInput source="properties.description" />
+      <LongTextInput source="properties.current-progressions" />
+      <LongTextInput source="properties.issues" />
+      <LongTextInput source="properties.observations" />
+      <LongTextInput source="properties.historial-data" />
+      <LongTextInput source="properties.cultural-references" />
+      <TextInput source="properties.theme_ids" /* Should be converted to referenceInput to RES_THEME */ />
+      <TextInput source="properties.keywords" />
+      <TextInput source="properties.landscape-entities" />
+      <TextInput source="properties.related-elements" />
+    </FormTab>
+
+    <FormTab label={t('resources.viewpoint.tabs.pictures')} path="pictures">
+      <ReferenceArrayField source="picture_ids" reference={RES_PICTURE} fullWidth>
+        <Datagrid>
+          <TextField source="owner" />
+          <TextField source="properties.meteo" />
+          <DateField source="date" />
+          <TextField source="state" />
+        </Datagrid>
+      </ReferenceArrayField>
+    </FormTab>
+  </TabbedForm>
 );
 
-export default ViewpointFields;
+export default translate(ViewpointFields);

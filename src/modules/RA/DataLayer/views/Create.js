9466d00b88d7bf6f76d582ec322cb9c68cf57d16
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-09-26 12:19:42 +0200

    Add BackToList button for each RA user/group Create/Edit forms

--- src/modules/RA/DataLayer/views/Create.js
+++ src/modules/RA/DataLayer/views/Create.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { Create } from 'react-admin';
 
 import DataLayerFormSelector from '../components/DataLayerFormSelector';
+import DefaultActions from '../../../../components/react-admin/DefaultActions';
 
 export const DataLayerCreate = props => (
-  <Create {...props}>
+  <Create
+    {...props}
+    actions={<DefaultActions />}
+  >
     <DataLayerFormSelector />
   </Create>
 );

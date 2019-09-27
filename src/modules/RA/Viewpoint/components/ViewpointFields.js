7342e63a9f2795afa1317c8196feea8a8960a23f
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-09-27 16:35:38 +0200

    Get cities & themes from back-end data

--- src/modules/RA/Viewpoint/components/ViewpointFields.js
+++ src/modules/RA/Viewpoint/components/ViewpointFields.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import Api from '@terralego/core/modules/Api';
 
 import {
+  AutocompleteArrayInput,
   Datagrid,
   DateField,
   DisabledInput,
   EditButton,
   FormTab,
   ImageField,
+  LinearProgress,
   LongTextInput,
   NumberInput,
   ReferenceArrayField,
@@ -19,57 +22,99 @@ import RichTextInput from 'ra-input-rich-text';
 import { RES_PICTURE } from '../../ra-modules';
 import MapPointInput from '../../../../components/react-admin/MapPointInput';
 
-const ViewpointFields = ({ edit = false, translate: t, ...props }) => (
-  <TabbedForm {...props}>
-    <FormTab label="resources.viewpoint.tabs.general">
-      {edit && <DisabledInput source="id" />}
-      <TextInput source="label" />
+const ViewpointFields = ({ edit = false, translate: t, ...props }) => {
+  const [remoteChoices, setRemoteChoices] = React.useState([]);
+  const [waiting, setWaiting] = React.useState(false);
 
-      <TextInput source="address" />
-      <SelectInput source="city_id" choices={[]} />
-      <NumberInput source="geometry.coordinates[0]" />
-      <NumberInput source="geometry.coordinates[1]" />
+  const getRemoteData = async () => {
+    setWaiting(true);
 
-      <MapPointInput source="geometry.coordinates" />
-    </FormTab>
+    const { themes, cities } = await Api.request('/viewpoints/filters/');
 
-    <FormTab label="resources.viewpoint.tabs.repeat" path="repeat">
-      <TextInput source="properties.altitude" />
-      <TextInput source="properties.hauteur" />
-      <TextInput source="properties.orientation" />
-      <TextInput source="properties.focale_35mm" />
-      <TextInput source="properties.focale_objectif" />
-      <SelectInput source="properties.frequency" choices={[]} />
-      <SelectInput source="properties.difficulty" choices={[]} />
-      <TextInput source="properties.note" />
-    </FormTab>
+    setRemoteChoices({
+      themes: themes.map(theme => ({ id: theme, name: theme })),
+      cities: cities.map(city => ({ id: city, name: city })),
+    });
 
-    <FormTab label="resources.viewpoint.tabs.landscape" path="landscape">
-      <RichTextInput source="properties.description" />
-      <LongTextInput source="properties.current-progressions" />
-      <LongTextInput source="properties.issues" />
-      <LongTextInput source="properties.observations" />
-      <LongTextInput source="properties.historial-data" />
-      <LongTextInput source="properties.cultural-references" />
-      <TextInput source="properties.theme_ids" /* Should be converted to referenceInput to RES_THEME */ />
-      <TextInput source="properties.keywords" />
-      <TextInput source="properties.landscape-entities" />
-      <TextInput source="properties.related-elements" />
-    </FormTab>
+    setWaiting(false);
+  };
 
-    <FormTab label="resources.viewpoint.tabs.pictures" path="pictures">
-      <ReferenceArrayField source="picture_ids" reference={RES_PICTURE} fullWidth>
-        <Datagrid rowClick="edit">
-          <TextField source="owner" />
-          <TextField source="properties.meteo" />
-          <DateField source="date" />
-          <TextField source="state" />
-          <ImageField source="file" />
-          <EditButton />
-        </Datagrid>
-      </ReferenceArrayField>
-    </FormTab>
-  </TabbedForm>
-);
+  React.useEffect(() => {
+    if (edit) {
+      getRemoteData();
+    }
+  }, [edit]);
+
+  return (
+    <TabbedForm {...props}>
+      <FormTab label="resources.viewpoint.tabs.general">
+        {edit && <DisabledInput source="id" />}
+        <TextInput source="label" />
+
+        <TextInput source="address" />
+
+        {waiting && <><LinearProgress /></>}
+        {!waiting && (
+          <SelectInput
+            translateChoice={false}
+            source="properties.commune"
+            choices={remoteChoices.cities}
+          />
+        )}
+
+        <NumberInput source="geometry.coordinates[0]" />
+        <NumberInput source="geometry.coordinates[1]" />
+
+        <MapPointInput source="geometry.coordinates" />
+      </FormTab>
+
+      <FormTab label="resources.viewpoint.tabs.repeat" path="repeat">
+        <TextInput source="properties.altitude" />
+        <TextInput source="properties.hauteur" />
+        <TextInput source="properties.orientation" />
+        <TextInput source="properties.focale_35mm" />
+        <TextInput source="properties.focale_objectif" />
+        <SelectInput source="properties.frequency" choices={[]} />
+        <SelectInput source="properties.difficulty" choices={[]} />
+        <TextInput source="properties.note" />
+      </FormTab>
+
+      <FormTab label="resources.viewpoint.tabs.landscape" path="landscape">
+        <RichTextInput source="properties.description" />
+        <LongTextInput source="properties.current-progressions" />
+        <LongTextInput source="properties.issues" />
+        <LongTextInput source="properties.observations" />
+        <LongTextInput source="properties.historial-data" />
+        <LongTextInput source="properties.cultural-references" />
+
+        {waiting && <><LinearProgress /></>}
+        {!waiting && (
+          <AutocompleteArrayInput
+            translateChoice={false}
+            source="properties.themes"
+            choices={remoteChoices.themes}
+          />
+        )}
+
+        <TextInput source="properties.keywords" />
+        <TextInput source="properties.landscape-entities" />
+        <TextInput source="properties.related-elements" />
+      </FormTab>
+
+      <FormTab label="resources.viewpoint.tabs.pictures" path="pictures">
+        <ReferenceArrayField source="picture_ids" reference={RES_PICTURE} fullWidth>
+          <Datagrid rowClick="edit">
+            <TextField source="owner" />
+            <TextField source="properties.meteo" />
+            <DateField source="date" />
+            <TextField source="state" />
+            <ImageField source="file" />
+            <EditButton />
+          </Datagrid>
+        </ReferenceArrayField>
+      </FormTab>
+    </TabbedForm>
+  );
+};
 
 export default ViewpointFields;

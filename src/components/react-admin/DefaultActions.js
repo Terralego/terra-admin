3a692d674fb8af428c6be7fd7a757d4b324ebce1
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2019-10-08 17:18:01 +0200

    Add referrer info for picture edition

--- src/components/react-admin/DefaultActions.js
+++ src/components/react-admin/DefaultActions.js
@@ -1,15 +1,38 @@
 import React from 'react';
-
+import { withRouter, Link } from 'react-router-dom';
 import { CardActions, ListButton } from 'react-admin';
 
-const DefaultActions = ({ basePath }) => (
-  <CardActions>
-    <ListButton
-      basePath={basePath}
-      variant="outlined"
-      label="ra.action.back-to-list"
-    />
-  </CardActions>
-);
+import IconArrowBack from '@material-ui/icons/ArrowBack'; // eslint-disable-line import/no-extraneous-dependencies
+import Button from '@material-ui/core/Button'; // eslint-disable-line import/no-extraneous-dependencies
 
-export default DefaultActions;
+import compose from '../../utils/compose';
+
+const DefaultActions = ({
+  basePath,
+  location: { state: { referrer } = {} },
+}) => {
+  return (
+    <CardActions>
+      {!referrer && (
+        <ListButton
+          basePath={basePath}
+          variant="outlined"
+          label="ra.action.back-to-list"
+        />
+      )}
+      {referrer && (
+        <Button
+          component={Link}
+          to={{
+            pathname: referrer,
+          }}
+          label="ra.action.back"
+        >
+          <IconArrowBack />
+        </Button>
+      )}
+    </CardActions>
+  );
+};
+
+export default compose(withRouter)(DefaultActions);

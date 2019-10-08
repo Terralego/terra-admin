5899bb410f5f6253b7bedd047e3e2b4ae477777e
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2019-10-08 14:46:32 +0200

    Merge old and new forms

--- src/modules/RA/Picture/components/PictureFields.js
+++ src/modules/RA/Picture/components/PictureFields.js
@@ -22,6 +22,7 @@ import { withStyles } from '@material-ui/core/styles'; // eslint-disable-line im
 import { RES_VIEWPOINT } from '../../ra-modules';
 import MapPointInput from '../../../../components/react-admin/MapPointInput';
 import compose from '../../../../utils/compose';
+import { withMapConfig } from '../../../../hoc/withAppSettings';
 
 const styles = {
   inline: {
@@ -35,29 +36,76 @@ const Br = () => <br />;
 const PictureFields = ({ edit, classes, mapConfig, ...props }) => (
   <TabbedForm {...props}>
     <FormTab label="resources.picture.tabs.metadata">
-      <TextInput source="label" formClassName={classes.inline} />
+      <ReferenceInput
+        source="viewpoint"
+        reference={RES_VIEWPOINT}
+        formClassName={classes.inline}
+      >
+        <SelectInput optionText="label" />
+      </ReferenceInput>
       <TextInput source="properties.index" formClassName={classes.inline} />
       <DateTimeInput source="date" showTime />
+      <DateInput source="date" />
 
-      <br />
+      <Br />
 
-      {edit && <DisabledInput source="owner.properties.name" formClassName={classes.inline} />}
-      <br />
-      <TextInput source="properties.camera_brand" formClassName={classes.inline} />
-      <TextInput source="properties.camera_model" formClassName={classes.inline} />
-      <TextInput source="properties.focale_35mm" formClassName={classes.inline} />
+      {edit && (
+        <DisabledInput
+          source="owner.properties.name"
+          formClassName={classes.inline}
+        />
+      )}
+
+      <Br />
+
+      <TextInput
+        source="properties.camera_brand"
+        formClassName={classes.inline}
+      />
+      <TextInput
+        source="properties.camera_model"
+        formClassName={classes.inline}
+      />
+      <TextInput
+        source="properties.focale_35mm"
+        formClassName={classes.inline}
+      />
       <TextInput source="properties.meteo" />
 
       <Br />
 
+      <TextInput source="remarks" validate={required()} />
       <LongTextInput source="properties.observations" />
 
       <Br />
-
-      <ImageField title="properties.index" source="file.full" />
+      <ImageInput source="file" accept="image/*">
+        <ImageField source="thumbnail" />
+      </ImageInput>
     </FormTab>
 
     <FormTab label="resources.picture.tabs.repeat" path="repeat">
+      <Br />
+
+      <TextInput source="properties.altitude" formClassName={classes.inline} />
+      <TextInput source="properties.hauteur" formClassName={classes.inline} />
+      <TextInput
+        source="properties.orientation"
+        formClassName={classes.inline}
+      />
+
+      <Br />
+
+      <TextInput
+        source="properties.focale_35mm"
+        formClassName={classes.inline}
+      />
+      <TextInput
+        source="properties.focale_objectif"
+        formClassName={classes.inline}
+      />
+
+      <Br />
+
       <NumberInput
         source="geometry.coordinates[1]"
         formClassName={classes.inline}
@@ -67,51 +115,7 @@ const PictureFields = ({ edit, classes, mapConfig, ...props }) => (
         formClassName={classes.inline}
       />
 
-      <Br />
-
-      <TextInput source="properties.altitude" formClassName={classes.inline} />
-      <TextInput source="properties.hauteur" formClassName={classes.inline} />
-      <TextInput source="properties.orientation" formClassName={classes.inline} />
-
-      <Br />
-
-      <TextInput source="properties.focale_35mm" formClassName={classes.inline} />
-      <TextInput source="properties.focale_objectif" formClassName={classes.inline} />
-
-      <Br />
-      <MapPointInput source="geometry.coordinates" />
-    </FormTab>
-
-    {/*  */}
-    {/*  */}
-
-    <FormTab label="jpa.general">
-      <ReferenceInput source="viewpoint" reference={RES_VIEWPOINT}>
-        <SelectInput optionText="label" />
-      </ReferenceInput>
-      <TextInput source="properties.index" />
-      <TextInput source="remarks" validate={required()} />
-      <DateInput source="date" />
-
-      <ImageInput source="file" accept="image/*">
-        <ImageField source="thumbnail" />
-      </ImageInput>
-    </FormTab>
-
-    <FormTab label="jpa.details">
-      <TextInput source="properties.meteo" />
-      <TextInput source="properties.hauteur" />
-      <TextInput source="properties.altitude" />
-      <TextInput source="properties.focale_35mm" />
-      <TextInput source="properties.orientation" />
-      <TextInput source="properties.camera_brand" />
-      <TextInput source="properties.camera_model" />
-      <TextInput source="properties.focale_objectif" />
-      <TextInput source="properties.photographer" />
-
-      {/* <ReferenceInput source="properties.photographer" reference={RES_USER}>
-        <SelectInput optionText="email" />
-      </ReferenceInput> */}
+      <MapPointInput source="geometry.coordinates" center={mapConfig.center} />
     </FormTab>
   </TabbedForm>
 );
@@ -125,5 +129,6 @@ PictureFields.defaultProps = {
 };
 
 export default compose(
-  withStyles(styles),
+  withMapConfig,
+  withStyles(styles)
 )(PictureFields);

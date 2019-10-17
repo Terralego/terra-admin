304b68d3051791ff5543be07524fedb828a89c2b
Aline <chetta.aline@gmail.com>
2019-10-17 15:23:19 +0200

    Specific slug control

--- src/modules/RA/Scene/components/SceneForm.js
+++ src/modules/RA/Scene/components/SceneForm.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+/* eslint-disable import/no-extraneous-dependencies */
+import { change } from 'redux-form';
+
 import {
   DisabledInput,
   ImageInput,
@@ -7,27 +10,70 @@ import {
   SelectInput,
   SimpleForm,
   TextInput,
+  FormDataConsumer,
+  REDUX_FORM_NAME,
 } from 'react-admin';
+import {
+  convertToSlug,
+  isObjectEmpty,
+} from '../../../../utils/react-admin/helper';
 
+const Br = () => <br />;
 
-const SceneForm = ({ edit = false, ...props }) => (
-  <SimpleForm {...props}>
-    {edit && <DisabledInput source="id" />}
+const SceneForm = ({ edit = false, ...props }) => {
+  const { record } = props;
+  return (
+    <SimpleForm {...props}>
+      {edit && <DisabledInput source="id" />}
 
-    <TextInput source="name" label="view.form.name" />
-    <TextInput source="slug" label="view.form.slug" />
-    <SelectInput
-      label="view.form.category"
-      source="category"
-      choices={[
-        { id: 'map', name: 'map' },
-        { id: 'story', name: 'story' },
-      ]}
-    />
-    <ImageInput source="custom_icon" label="view.form.icon">
-      <ImageField source="url" />
-    </ImageInput>
-  </SimpleForm>
-);
+      {isObjectEmpty(record) ? (
+        <>
+          <FormDataConsumer>
+            {({ dispatch, ...rest }) => (
+              <TextInput
+                source="name"
+                label="view.form.name"
+                onChange={value => {
+                  if (!value) return;
+                  const slugGenerated = convertToSlug(value.target.value);
+                  dispatch(change(REDUX_FORM_NAME, 'slug', slugGenerated));
+                }}
+                {...rest}
+              />
+            )}
+          </FormDataConsumer>
+          <Br />
+          <DisabledInput
+            source="slug"
+            label="view.form.slug"
+          />
+        </>
+      ) : (
+        <>
+          <TextInput source="name" label="view.form.name" />
+          <Br />
+          <TextInput source="slug" label="view.form.slug" />
+        </>
+      )}
+
+      <SelectInput
+        label="view.form.category"
+        source="category"
+        choices={[
+          {
+            id: 'map',
+            name: 'map',
+          }, {
+            id: 'story',
+            name: 'story',
+          },
+        ]}
+      />
+      <ImageInput source="custom_icon" label="view.form.icon">
+        <ImageField source="url" />
+      </ImageInput>
+    </SimpleForm>
+  );
+};
 
 export default SceneForm;
---
+++ src/utils/react-admin/helper.js
@@ -0,0 +1,12 @@
+export const convertToSlug = value => {
+  return value
+    .toLocaleLowerCase()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/ /g, '-')
+    .replace(/[^a-z0-9.-]/g, '');
+};
+
+export const isObjectEmpty = obj => Object.keys(obj).length === 0 && obj.constructor === Object;
+
+export default { convertToSlug, isObjectEmpty };

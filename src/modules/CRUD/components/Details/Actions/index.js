3a051da2a2f1c079762c7806bb001f320458087a
Florian Sommariva <florian.sommariva@makina-corpus.com>
2019-10-21 14:33:07 +0200

    Refacto HOC Details/Actions/index.js by using compose() method

--- src/modules/CRUD/components/Details/Actions/index.js
+++ src/modules/CRUD/components/Details/Actions/index.js
@@ -4,32 +4,36 @@ import { connectAuthProvider } from '@terralego/core/modules/Auth';
 
 import { connectCRUDProvider } from '../../../services/CRUDProvider';
 import { getView } from '../../../services/CRUD';
+import compose from '../../../../../utils/compose';
 
 import Actions from './Actions';
 
-export default withRouter(
-  connectAuthProvider(({
-    authenticated,
-    user,
-  }, { displayUpdate, displayDelete }) => {
-    const permissions = authenticated ? user.permissions : [];
-    return {
-      displayDelete: displayDelete && permissions.includes('terra_geocrud.can_delete_feature'),
-      displayUpdate: displayUpdate && permissions.includes('terra_geocrud.can_change_feature'),
-    };
-  })(
-    connectCRUDProvider(({
-      getSettings,
-      settings,
-      deleteFeature,
-    }, {
-      match: { params: { layer } },
-    }) => ({
-      getSettings,
-      deleteFeature,
-      view: getView(settings, layer),
-    }))(
-      withNamespaces()(Actions),
-    ),
-  ),
-);
+const authProvider = ({
+  authenticated,
+  user,
+}, { displayUpdate, displayDelete }) => {
+  const permissions = authenticated ? user.permissions : [];
+  return {
+    displayDelete: displayDelete && permissions.includes('terra_geocrud.can_delete_feature'),
+    displayUpdate: displayUpdate && permissions.includes('terra_geocrud.can_change_feature'),
+  };
+};
+
+const CRUDPRovider = ({
+  getSettings,
+  settings,
+  deleteFeature,
+}, {
+  match: { params: { layer } },
+}) => ({
+  getSettings,
+  deleteFeature,
+  view: getView(settings, layer),
+});
+
+export default compose(
+  withRouter,
+  connectAuthProvider(authProvider),
+  connectCRUDProvider(CRUDPRovider),
+  withNamespaces(),
+)(Actions);

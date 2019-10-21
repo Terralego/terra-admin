bbe547e19108943217fded1fc26a3215aff68c72
Florian Sommariva <florian.sommariva@makina-corpus.com>
2019-10-21 14:32:37 +0200

    Refacto HOC Details/Edit/index.js by using compose() method

--- src/modules/CRUD/components/Details/Edit/index.js
+++ src/modules/CRUD/components/Details/Edit/index.js
@@ -4,41 +4,45 @@ import { connectAuthProvider } from '@terralego/core/modules/Auth';
 
 import { connectCRUDProvider } from '../../../services/CRUDProvider';
 import { getView, getLayersPaints } from '../../../services/CRUD';
+import compose from '../../../../../utils/compose';
 
 import Edit from './Edit';
 
-export default withRouter(
-  connectAuthProvider(({
-    authenticated,
-    user,
-  }) => {
-    const permissions = authenticated ? user.permissions : [];
-    return {
-      displayAddFeature: permissions.includes('terra_geocrud.can_add_feature'),
-      displayChangeFeature: permissions.includes('terra_geocrud.can_change_feature'),
-    };
-  })(
-    connectCRUDProvider(({
-      getSettings,
-      getFeaturesList,
-      settings,
-      map,
-      feature,
-      saveFeature,
-    }, {
-      match: { params: { layer, id } },
-    }) => ({
-      getSettings,
-      getFeaturesList,
-      map,
-      feature: feature[id] || {},
-      saveFeature,
-      view: getView(settings, layer),
-      layerPaint: getLayersPaints(settings).find(item => item['source-layer'] === layer) || {},
-      paramLayer: layer,
-      paramId: id,
-    }))(
-      withNamespaces()(Edit),
-    ),
-  ),
-);
+const authProvider = ({
+  authenticated,
+  user,
+}) => {
+  const permissions = authenticated ? user.permissions : [];
+  return {
+    displayAddFeature: permissions.includes('terra_geocrud.can_add_feature'),
+    displayChangeFeature: permissions.includes('terra_geocrud.can_change_feature'),
+  };
+};
+
+const CRUDPRovider = ({
+  getSettings,
+  getFeaturesList,
+  settings,
+  map,
+  feature,
+  saveFeature,
+}, {
+  match: { params: { layer, id } },
+}) => ({
+  getSettings,
+  getFeaturesList,
+  map,
+  feature: feature[id] || {},
+  saveFeature,
+  view: getView(settings, layer),
+  layerPaint: getLayersPaints(settings).find(item => item['source-layer'] === layer) || {},
+  paramLayer: layer,
+  paramId: id,
+});
+
+export default compose(
+  withRouter,
+  connectAuthProvider(authProvider),
+  connectCRUDProvider(CRUDPRovider),
+  withNamespaces(),
+)(Edit);

964c4fc1cd640611409593f41db2dfeafd9da374
Florian Sommariva <florian.sommariva@makina-corpus.com>
2019-10-21 14:31:13 +0200

    Refacto HOC view/Map/index.js by using compose() method

--- src/modules/CRUD/views/Map/index.js
+++ src/modules/CRUD/views/Map/index.js
@@ -3,47 +3,47 @@ import { withNamespaces } from 'react-i18next';
 import { connectAuthProvider } from '@terralego/core/modules/Auth';
 
 import { connectCRUDProvider } from '../../services/CRUDProvider';
+import compose from '../../../../utils/compose';
 
 import Map from './Map';
 
-export default withRouter(
-  connectAuthProvider(({
-    authenticated,
-    user,
-  }) => {
-    const permissions = authenticated ? user.permissions : [];
-    return {
-      displayViewFeature: permissions.includes('terra_geocrud.can_view_feature'),
-    };
-  })(
-    connectCRUDProvider(({
-      getSettings,
-      getMapConfig,
-      settings,
-      mapConfig,
-      setMap,
-      map,
-      mapIsResizing,
-      feature,
-      errors,
-    }, {
-      match: {
-        params: {
-          id,
-        },
-      },
-    }) => ({
-      getSettings,
-      getMapConfig,
-      settings,
-      mapConfig,
-      setMap,
-      map,
-      mapIsResizing,
-      feature: feature[id] || {},
-      errors,
-    }))(
-      withNamespaces()(Map),
-    ),
-  ),
-);
+const authProvider = ({
+  authenticated,
+  user,
+}) => {
+  const permissions = authenticated ? user.permissions : [];
+  return {
+    displayViewFeature: permissions.includes('terra_geocrud.can_view_feature'),
+  };
+};
+
+const CRUDPRovider = ({
+  getSettings,
+  settings,
+  setMap,
+  map,
+  mapIsResizing,
+  feature,
+  errors,
+}, {
+  match: {
+    params: {
+      id,
+    },
+  },
+}) => ({
+  getSettings,
+  settings,
+  setMap,
+  map,
+  mapIsResizing,
+  feature: feature[id] || {},
+  errors,
+});
+
+export default compose(
+  withRouter,
+  connectAuthProvider(authProvider),
+  connectCRUDProvider(CRUDPRovider),
+  withNamespaces(),
+)(Map);

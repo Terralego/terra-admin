b566bb33eb8fc0bdc8eeeb84a140511f60509e9a
Florian Sommariva <florian.sommariva@makina-corpus.com>
2019-10-22 15:50:26 +0200

    Isolate details Header to a dedicated component

---
+++ src/modules/CRUD/components/Details/Header/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import DownloadButtons from '../DownloadButtons';
+
+const Header = ({ title, documents }) => (
+  <div className="details__header">
+    <h2 className="details__title">{title}</h2>
+    {documents && (
+      <DownloadButtons
+        className="details__templates"
+        documents={documents}
+      />
+    )}
+  </div>
+);
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+  documents: PropTypes.arrayOf(
+    PropTypes.shape({
+      template_name: PropTypes.string,
+      download_url: PropTypes.string,
+      template_file: PropTypes.string,
+    }),
+  ),
+};
+
+Header.defaultProps = {
+  documents: undefined,
+};
+
+export default Header;

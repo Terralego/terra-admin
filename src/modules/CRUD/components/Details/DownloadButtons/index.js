e6da03487c04c7d3dfca164bc3a79c04e431b2bd
Florian Sommariva <florian.sommariva@makina-corpus.com>
2019-11-04 14:57:36 +0100

    Display DownloadButtons as a dropdown

--- src/modules/CRUD/components/Details/DownloadButtons/index.js
+++ src/modules/CRUD/components/Details/DownloadButtons/index.js
@@ -1,3 +1,8 @@
+import { withNamespaces } from 'react-i18next';
+
+import compose from '../../../../../utils/compose';
 import DownloadButtons from './DownloadButtons';
 
-export default DownloadButtons;
+export default compose(
+  withNamespaces(),
+)(DownloadButtons);

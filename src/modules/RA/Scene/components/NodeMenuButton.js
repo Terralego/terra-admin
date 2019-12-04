fe6df2c2ab334c7b6f9a2e2214f8a9c9b20a10b7
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2019-12-04 18:15:19 +0100

    Use a modal window for editing scene tree nodes

--- src/modules/RA/Scene/components/NodeMenuButton.js
+++ src/modules/RA/Scene/components/NodeMenuButton.js
@@ -3,20 +3,43 @@ import React from 'react';
 import { addNodeUnderParent, removeNodeAtPath } from 'react-sortable-tree';
 
 /* eslint-disable import/no-extraneous-dependencies */
+import Button from '@material-ui/core/Button';
+import EditIcon from '@material-ui/icons/Edit';
 import IconButton from '@material-ui/core/IconButton';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import Modal from '@material-ui/core/Modal';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 /* eslint-enable */
 
+const style = {
+  modal: {
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    position: 'absolute',
+    width: 500,
+    backgroundColor: 'white',
+    padding: '1em',
+  },
+};
+
 const NodeMenuButton = ({ treeData, setTreeData, path, isGroup }) => {
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const [modalOpen, setModalOpen] = React.useState(false);
 
   const handleClick = ({ currentTarget }) => setAnchorEl(currentTarget);
-  const handleClose = () => setAnchorEl(null);
+  const closeMenu = () => setAnchorEl(null);
+
+  const openModal = () => {
+    closeMenu();
+    setModalOpen(true);
+  };
+  const closeModal = () => setModalOpen(false);
 
   const newSubItem = newNode => () => {
-    handleClose();
+    closeMenu();
+    closeModal();
     setTreeData(addNodeUnderParent({
       treeData,
       parentKey: path[path.length - 1],
@@ -27,7 +50,8 @@ const NodeMenuButton = ({ treeData, setTreeData, path, isGroup }) => {
   };
 
   const deleteItem = () => {
-    handleClose();
+    closeMenu();
+    closeModal();
     setTreeData(removeNodeAtPath({
       treeData,
       path,
@@ -40,15 +64,23 @@ const NodeMenuButton = ({ treeData, setTreeData, path, isGroup }) => {
 
   return (
     <>
+      <IconButton onClick={openModal}><EditIcon /></IconButton>
+
+      <Modal open={modalOpen} onClose={closeModal}>
+        <div style={style.modal}>
+          {isGroup && <Button onClick={newSubLayer}>Ajouter une couche</Button>}
+          {isGroup && <Button onClick={newSubGroup}>Ajouter un groupe</Button>}
+          <Button onClick={deleteItem}>Supprimer</Button>
+        </div>
+      </Modal>
+
       <IconButton onClick={handleClick}><MoreVertIcon /></IconButton>
-      <Menu
-        anchorEl={anchorEl}
-        onClose={handleClose}
-        open={!!anchorEl}
-      >
+
+      <Menu anchorEl={anchorEl} onClose={closeMenu} open={!!anchorEl}>
         {isGroup && <MenuItem onClick={newSubLayer}>Ajouter une couche</MenuItem>}
         {isGroup && <MenuItem onClick={newSubGroup}>Ajouter un groupe</MenuItem>}
         <MenuItem onClick={deleteItem}>Supprimer</MenuItem>
+        <MenuItem onClick={openModal}>Editer</MenuItem>
       </Menu>
     </>
   );

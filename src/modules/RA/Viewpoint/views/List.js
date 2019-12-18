2d8c959580fd13b210577ebb48f900ca032705a1
jmr <jmr@makina-corpus.com>
2019-12-18 15:34:42 +0100

    Add commune select and filter for viewpoints list

--- src/modules/RA/Viewpoint/views/List.js
+++ src/modules/RA/Viewpoint/views/List.js
@@ -1,13 +1,49 @@
 import React from 'react';
-import { List } from 'react-admin';
+import {
+  Filter,
+  List,
+  SelectInput,
+  TextInput,
+} from 'react-admin';
 
 import CommonBulkActionButtons from '../../../../components/react-admin/CommonBulkActionButtons';
 import GridList from '../../../../components/react-admin/GridList';
+import {fetchFilterOptions} from "../../ra-modules";
+
+
+const ListFilters = props => {
+  const [cities, setCities] = React.useState([]);
+
+  const setCitiesNames = async () => {
+    const data = await fetchFilterOptions();
+    const citiesNames = data.cities.map(city => ({name: city, id: city}));
+
+    setCities({
+      names: citiesNames,
+    });
+  };
+
+  React.useEffect(() => {
+    setCitiesNames();
+  }, []);
+
+  return (
+    <Filter {...props}>
+      <TextInput label="ra.action.search" source="search" alwaysOn />
+      <SelectInput
+        source="commune"
+        label="commune"
+        choices={cities.names}
+      />
+    </Filter>
+  );
+};
 
 export const ViewpointList = props => (
   <List
     {...props}
     exporter={false}
+    filters={<ListFilters />}
     bulkActionButtons={<CommonBulkActionButtons />}
   >
     <GridList />

23f16e7de927b1bdaa024c218310f151127e9453
Florian Sommariva <florian.sommariva@makina-corpus.com>
2020-01-14 14:58:01 +0100

    Create a provider to store all user settings

--- src/modules/CRUD/index.js
+++ src/modules/CRUD/index.js
@@ -5,23 +5,26 @@ import NavLayout from '../../components/NavLayout';
 import Nav from './components/Nav';
 import Routing from '../../components/Routing';
 import CRUDProvider from './services/CRUDProvider';
+import UserSettingsProvider from './services/UserSettingsProvider';
 
 import './styles.scss';
 
 const { routes } = config;
 
 export const CRUD = () => (
-  <CRUDProvider>
-    <div className="CRUD">
-      <NavLayout nav={<Nav />}>
-        <div className="CRUD-main">
-          <Routing
-            routes={routes}
-          />
-        </div>
-      </NavLayout>
-    </div>
-  </CRUDProvider>
+  <UserSettingsProvider>
+    <CRUDProvider>
+      <div className="CRUD">
+        <NavLayout nav={<Nav />}>
+          <div className="CRUD-main">
+            <Routing
+              routes={routes}
+            />
+          </div>
+        </NavLayout>
+      </div>
+    </CRUDProvider>
+  </UserSettingsProvider>
 );
 
 CRUD.config = config;

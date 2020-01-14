87fb5a6aa20ae51996ecbd971eb33079694f9fb1
Florian Sommariva <florian.sommariva@makina-corpus.com>
2020-01-14 14:58:01 +0100

    Create a provider to store all user settings

---
+++ src/modules/CRUD/services/UserSettingsProvider.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import connect from 'react-ctx-connect';
+
+export const context = React.createContext({});
+export const connectUserSettingsProvider = connect(context);
+
+const { Provider } = context;
+
+export const UserSettingsProvider = ({ children }) => {
+  const [pageSize, setPageSize] = useState(10);
+
+  const value = {
+    pageSize,
+    setPageSize,
+  };
+
+  return (
+    <Provider value={value}>
+      {children}
+    </Provider>
+  );
+};
+
+export default UserSettingsProvider;

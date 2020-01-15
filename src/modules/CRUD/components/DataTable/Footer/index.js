39f39dff9f3897b2db8bd2f01f9487e17748dcd6
Florian Sommariva <florian.sommariva@makina-corpus.com>
2020-01-15 14:10:57 +0100

    Use abstract 'withPageSize' connector

--- src/modules/CRUD/components/DataTable/Footer/index.js
+++ src/modules/CRUD/components/DataTable/Footer/index.js
@@ -1,9 +1,9 @@
 import { withNamespaces } from 'react-i18next';
-import { connectUserSettingsProvider } from '../../../services/UserSettingsProvider';
+import { withPageSize } from '../../../services/UserSettingsProvider';
 import compose from '../../../../../utils/compose';
 import Footer from './Footer';
 
 export default compose(
-  connectUserSettingsProvider('pageSize', 'setPageSize'),
+  withPageSize(),
   withNamespaces(),
 )(Footer);

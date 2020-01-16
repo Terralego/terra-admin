1117d53daeb7235bceac8e94f0ae0111554a806b
Florian Sommariva <florian.sommariva@makina-corpus.com>
2020-01-16 13:44:27 +0100

    Create tableSize in UserSettingsProvider

--- src/modules/CRUD/services/UserSettingsProvider.js
+++ src/modules/CRUD/services/UserSettingsProvider.js
@@ -4,17 +4,24 @@ import connect from 'react-ctx-connect';
 export const context = React.createContext({});
 export const connectUserSettingsProvider = connect(context);
 
-export const withPageSize = () =>
-  connectUserSettingsProvider('pageSize', 'setPageSize');
+export const TABLE_MINIFIED = 'minified';
+export const TABLE_MEDIUM = 'medium';
+export const TABLE_FULL = 'full';
+
+export const withPageSize = () => connectUserSettingsProvider('pageSize', 'setPageSize');
+export const withTableSize = () => connectUserSettingsProvider('tableSize', 'setTableSize');
 
 const { Provider } = context;
 
 export const UserSettingsProvider = ({ children }) => {
   const [pageSize, setPageSize] = useState(10);
+  const [tableSize, setTableSize] = useState(TABLE_MEDIUM);
 
   const value = {
     pageSize,
     setPageSize,
+    tableSize,
+    setTableSize,
   };
 
   return (

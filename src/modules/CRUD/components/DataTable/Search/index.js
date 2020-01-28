b605d1214effaa1414668f0098d4729aecbe3838
Florian Sommariva <florian.sommariva@makina-corpus.com>
2020-01-28 10:30:07 +0100

    Create DataTable/Search component

---
+++ src/modules/CRUD/components/DataTable/Search/index.js
@@ -0,0 +1,30 @@
+import { withNamespaces } from 'react-i18next';
+import { withRouter } from 'react-router-dom';
+import { connectCRUDProvider } from '../../../services/CRUDProvider';
+import { withTableFilters } from '../../../services/UserSettingsProvider';
+import { getView } from '../../../services/CRUD';
+import compose from '../../../../../utils/compose';
+
+import Search from './Search';
+
+const CRUDPRoviderGetter = ({
+  featuresList,
+  getFeaturesList,
+  settings,
+}, {
+  match: { params: { layer } },
+}) => {
+  const { featureEndpoint } = getView(settings, layer);
+  return {
+    featureEndpoint,
+    featuresList,
+    getFeaturesList,
+  };
+};
+
+export default compose(
+  withRouter,
+  connectCRUDProvider(CRUDPRoviderGetter),
+  withTableFilters(),
+  withNamespaces(),
+)(Search);

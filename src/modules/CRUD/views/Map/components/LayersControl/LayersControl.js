095fed09848e8bf31d9ea285feb0c21d1419c16a
Florian Sommariva <florian.sommariva@makina-corpus.com>
2020-01-30 16:07:36 +0100

    Make the whole button of LayersControl clickable

--- src/modules/CRUD/views/Map/components/LayersControl/LayersControl.js
+++ src/modules/CRUD/views/Map/components/LayersControl/LayersControl.js
@@ -46,32 +46,32 @@ export class LayersControl extends AbstractMapControl {
       <Tooltip
         content={translate('CRUD.map.controls.layers.label')}
       >
-        <button
-          className="mapboxgl-ctrl-icon"
-          type="button"
-          aria-label={translate('CRUD.map.controls.layers.label')}
-        >
-          <Popover
-            className="popoverPos"
-            position={Position.BOTTOM_LEFT}
-            content={(
-              <div className="radioGroup">
-                {layers.map(({ title, id, displayOnMap = true }) => (
-                  <Checkbox
-                    className="bgLayer-radio"
-                    key={id}
-                    label={title}
-                    value={id}
-                    onChange={onChange}
-                    defaultChecked={displayOnMap}
-                  />
-                ))}
-              </div>
+        <Popover
+          className="popoverPos"
+          position={Position.BOTTOM_LEFT}
+          content={(
+            <div className="radioGroup">
+              {layers.map(({ title, id, displayOnMap = true }) => (
+                <Checkbox
+                  className="bgLayer-radio"
+                  key={id}
+                  label={title}
+                  value={id}
+                  onChange={onChange}
+                  defaultChecked={displayOnMap}
+                />
+              ))}
+            </div>
           )}
+        >
+          <button
+            className="mapboxgl-ctrl-icon"
+            type="button"
+            aria-label={translate('CRUD.map.controls.layers.label')}
           >
             <Icon icon="multi-select" />
-          </Popover>
-        </button>
+          </button>
+        </Popover>
       </Tooltip>
 
     );

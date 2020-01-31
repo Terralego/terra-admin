a12301f1f8020cd5cfc2179b2802230590b934a5
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2020-01-31 21:21:42 +0100

    Add translation to style field

--- src/modules/RA/DataLayer/components/StyleField.js
+++ src/modules/RA/DataLayer/components/StyleField.js
@@ -1,23 +1,30 @@
 import React from 'react';
 
+import { translate as RAtranslate } from 'react-admin';
+
 import { JSONInput } from '../../../../components/react-admin/JSONInput';
 
-const validate = value => {
-  if (!value) {
-    return 'empty value';
-  }
-  if (!value.type) {
-    return 'type not found';
-  }
-  return undefined;
+import compose from '../../../../utils/compose';
+
+const StyleField = ({ translate, ...props }) => {
+  const memoizedValidate = React.useCallback(value => {
+    if (!value) {
+      return translate('ra.input.style_field.empty_object');
+    }
+    if (!value.type) {
+      return translate('ra.input.style_field.missing_property', { property: 'type' });
+    }
+    return undefined;
+  },
+  [translate]);
+
+  return (
+    <JSONInput
+      {...props}
+      validate={memoizedValidate}
+    />
+  );
 };
 
-const StyleField = props => (
-  <JSONInput
-    {...props}
-    validate={validate}
-  />
-);
 
-
-export default StyleField;
+export default compose(RAtranslate)(StyleField);

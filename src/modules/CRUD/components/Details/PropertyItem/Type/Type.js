1a33cfe87fcff61bbe93425c1aba999d9a27306f
Florian Sommariva <florian.sommariva@makina-corpus.com>
2020-02-13 14:12:32 +0100

    Create Type component and manage StringType component

---
+++ src/modules/CRUD/components/Details/PropertyItem/Type/Type.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import StringType from './StringType';
+
+export const TYPES = {
+  number: StringType,
+  string: StringType,
+};
+
+export const getComponent = type => {
+  if (type in TYPES) {
+    return TYPES[type];
+  }
+
+  // eslint-disable-next-line no-console
+  console.warn(`type ${type} is invalid`);
+  return () => null;
+};
+
+const Type = props => {
+  const {
+    schema: {
+      type,
+    },
+  } = props;
+
+  const Component = getComponent(type);
+
+  return (
+    <Component {...props} />
+  );
+};
+
+Type.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  display_value: PropTypes.any,
+  schema: PropTypes.shape({
+    type: PropTypes.string,
+  }),
+  t: PropTypes.func,
+};
+
+Type.defaultProps = {
+  display_value: null,
+  schema: {
+    type: '',
+  },
+  t: text => text,
+};
+
+export default Type;

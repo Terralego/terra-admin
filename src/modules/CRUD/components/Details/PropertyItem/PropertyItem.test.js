655d330c368f38804d1694ef5696eecf422879a6
Florian Sommariva <florian.sommariva@makina-corpus.com>
2020-02-21 14:40:52 +0100

    Edit on the fly feature properties

--- src/modules/CRUD/components/Details/PropertyItem/PropertyItem.test.js
+++ src/modules/CRUD/components/Details/PropertyItem/PropertyItem.test.js
@@ -3,17 +3,37 @@ import renderer from 'react-test-renderer';
 
 import PropertyItem from './PropertyItem';
 
-jest.mock('./Type', () => props => <div {...props} />);
+jest.mock('./Edit', () => props => <div {...props}>Edit</div>);
+jest.mock('./Type', () => props => <div {...props}>Type</div>);
 
 const props = {
+  canViewFeature: true,
+  canUpdateFeature: true,
   value: {
     title: 'Foo',
   },
 };
 
-it('should render correctly', () => {
-  const tree = renderer.create((
-    <PropertyItem {...props} />
-  )).toJSON();
-  expect(tree).toMatchSnapshot();
+describe('should render correctly', () => {
+  it('active item', () => {
+    const tree = renderer.create((
+      <PropertyItem
+        {...props}
+        name="bar"
+        editedItem="bar"
+      />
+    )).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('inactive item', () => {
+    const tree = renderer.create((
+      <PropertyItem
+        {...props}
+        name="test"
+        editedItem="bar"
+      />
+    )).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });

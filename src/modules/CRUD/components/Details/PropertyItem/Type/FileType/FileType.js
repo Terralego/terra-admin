7a5eba6f5ed4a8595f54b670ec017f1f4ab70b72
Florian Sommariva <florian.sommariva@makina-corpus.com>
2020-02-21 14:37:09 +0100

    Manage files's array and set filename title to FileType component

--- src/modules/CRUD/components/Details/PropertyItem/Type/FileType/FileType.js
+++ src/modules/CRUD/components/Details/PropertyItem/Type/FileType/FileType.js
@@ -1,36 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { parseUrl } from 'query-string';
 import NoValue from '../../NoValue';
 
-const FileType = ({
-  display_value: displayValue,
-  schema: {
-    title,
-  },
-  type,
-}) => {
+const getFileName = src => {
+  const { url } = parseUrl(src);
+  return decodeURIComponent(url.split('/').pop());
+};
+
+const FileType = props => {
+  const {
+    display_value: displayValue,
+    schema: {
+      title,
+    },
+    type,
+  } = props;
   const { url } = displayValue;
   if (!url) {
     return <NoValue />;
   }
-  if (type === 'image') {
-    return <img src={url} alt={title} />;
-  }
   return (
-    <a
-      href={url}
-      rel="download noopener noreferrer"
-      target="_blank"
-    >
-      {title}
-    </a>
+    <>
+      {url.map((src => {
+        const text = getFileName(src) || title;
+        return (
+          (type === 'image')
+            ? <img key={src} src={src} alt={text} />
+            : (
+              <a
+                key={src}
+                href={src}
+                rel="download noopener noreferrer"
+                target="_blank"
+              >
+                {text}
+              </a>
+            )
+        );
+      }))}
+    </>
   );
 };
 
 FileType.propTypes = {
   display_value: PropTypes.shape({
     thumbnail: PropTypes.string,
-    url: PropTypes.string,
+    url: PropTypes.arrayOf(PropTypes.string),
   }),
   schema: PropTypes.shape({
     title: PropTypes.string,

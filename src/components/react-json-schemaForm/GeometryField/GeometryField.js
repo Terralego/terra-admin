6df4127dce6f5e84d781f68374fd011cb6cf5a00
Florian Sommariva <florian.sommariva@makina-corpus.com>
2020-03-13 16:35:15 +0100

    Improve GeometryField component

--- src/components/react-json-schemaForm/GeometryField/GeometryField.js
+++ src/components/react-json-schemaForm/GeometryField/GeometryField.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Message from '../../Message';
 import PointField from './PointField';
 
+const Fieldset = ({ t, children }) => (
+  <fieldset>
+    <legend>{t('jsonSchema.geometryField.title')}</legend>
+    <div className="form-group field">
+      {children}
+    </div>
+  </fieldset>
+);
+
 const GeometryField = props => {
-  const { formData: { type }, t } = props;
+  const { formData: { coordinates, type }, t } = props;
   if (type === 'Point') {
     return (
-      <PointField {...props} />
+      <Fieldset t={t}>
+        <PointField {...props} />
+      </Fieldset>
     );
   }
+  const action = !coordinates.length ? 'create' : 'edit';
   return (
-    <div>
-      {type && <p>{t('jsonSchema.geometryField.title')} <em>{type}</em>.</p>}
-      <p>{t('jsonSchema.geometryField.helper-edit')}</p>
-      <p>{t('jsonSchema.geometryField.helper-save')}</p>
-    </div>
+    <Fieldset t={t}>
+      <Message intent="primary">
+        <p>{t(`jsonSchema.geometryField.helper-${action}`, { type })}</p>
+        <div>{t('jsonSchema.geometryField.save')}</div>
+      </Message>
+    </Fieldset>
   );
 };
 

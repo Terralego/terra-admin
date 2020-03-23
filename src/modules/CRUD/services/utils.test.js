80ef8662d2b420b7c3454e0d32d3d78d729bed05
Florian Sommariva <florian.sommariva@makina-corpus.com>
2020-03-23 11:31:34 +0100

    Create utils functions to create JSON schema from geometry

--- src/modules/CRUD/services/utils.test.js
+++ src/modules/CRUD/services/utils.test.js
@@ -1,4 +1,114 @@
-import { getObjectOrderedValue, isTableObject, sanitizeCustomEndpoint } from './utils';
+import { getJSONSchemaFromGeom, getObjectOrderedValue, isTableObject, sanitizeCustomEndpoint } from './utils';
+
+
+describe('should get JSON schema from geom', () => {
+  it('with no values', () => {
+    const props = {
+      identifier: null,
+      geom: undefined,
+      geom_type: 0,
+      title: 'Foo',
+    };
+    expect(getJSONSchemaFromGeom(props, true)).toEqual({
+      display_value: {
+        type: 'Point',
+        coordinates: [],
+      },
+      method: 'POST',
+      schema: {
+        properties: {
+          type: {
+            title: 'Type',
+            type: 'string',
+          },
+          coordinates: {
+            items: {
+              type: 'number',
+            },
+            type: 'array',
+          },
+        },
+        required: ['type'],
+        title: 'Foo',
+        type: 'object',
+      },
+      ui_schema: {
+        'ui:field': 'geometry',
+        coordinates: {
+          'ui:options': {
+            addable: false,
+            orderable: false,
+            removable: false,
+          },
+        },
+        type: { 'ui:widget': 'hidden' },
+      },
+      title: 'Foo',
+      type: 'object',
+      value: { type: 'Point', coordinates: [] },
+    });
+  });
+
+  it('with values', () => {
+    const props = {
+      identifier: 'UUID',
+      geom: {
+        type: 'LineString',
+        coordinates: [[46, 2]],
+      },
+      geom_type: 3,
+      title: 'Foo',
+    };
+    expect(getJSONSchemaFromGeom(props)).toEqual({
+      display_value: {
+        type: 'LineString',
+        coordinates: [[46, 2]],
+      },
+      method: 'PATCH',
+      schema: {
+        properties: {
+          type: {
+            title: 'Type',
+            type: 'string',
+          },
+          coordinates: {
+            items: {
+              items: {
+                type: 'number',
+              },
+              type: 'array',
+            },
+            type: 'array',
+          },
+        },
+        required: ['type', 'coordinates'],
+        title: 'Foo',
+        type: 'object',
+      },
+      ui_schema: {
+        'ui:field': 'geometry',
+        coordinates: {
+          items: {
+            'ui:options': {
+              addable: false,
+              orderable: false,
+              removable: false,
+            },
+          },
+          'ui:options': {
+            addable: false,
+            orderable: false,
+            removable: false,
+          },
+        },
+        type: { 'ui:widget': 'hidden' },
+      },
+      title: 'Foo',
+      type: 'object',
+      value: { type: 'LineString', coordinates: [[46, 2]] },
+    });
+  });
+});
 
 it('should get object with ordered value', () => {
   const arrayOrder = ['1', 'two', 'E', '4'];

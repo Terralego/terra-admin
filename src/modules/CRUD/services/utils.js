2fe82d6e0b4f8865774e4066bb5d8369e8c01854
Florian Sommariva <florian.sommariva@makina-corpus.com>
2020-03-23 11:31:34 +0100

    Create utils functions to create JSON schema from geometry

--- src/modules/CRUD/services/utils.js
+++ src/modules/CRUD/services/utils.js
@@ -1,3 +1,15 @@
+import { geomTypes } from '../../RA/DataSource/index';
+
+import {
+  POINT,
+  LINESTRING,
+  POLYGON,
+  MULTI_POINT,
+  MULTI_LINESTRING,
+  MULTI_POLYGON,
+  GEOMETRY_COLLECTION,
+} from '../../../utils/geom';
+
 export const getObjectOrderedValue = (objectValues, arrayOrder = []) => {
   if (!objectValues) {
     return {};
@@ -25,4 +37,94 @@ export const sanitizeCustomEndpoint = str => {
   return str;
 };
 
-export default { getObjectOrderedValue, isTableObject, sanitizeCustomEndpoint };
+
+const getSchemaCoordinates = coordinates => {
+  if (!Array.isArray(coordinates[0])) {
+    return { type: 'number' };
+  }
+  return {
+    items: getSchemaCoordinates(coordinates[0]),
+    type: 'array',
+  };
+};
+
+const getUISchemaCoordinates = coordinates => {
+  const uiOptions = {
+    addable: false,
+    orderable: false,
+    removable: false,
+  };
+  if (!Array.isArray(coordinates[0])) {
+    return {
+      'ui:options': uiOptions,
+    };
+  }
+  return {
+    items: getUISchemaCoordinates(coordinates[0]),
+    'ui:options': uiOptions,
+  };
+};
+
+const getDefaultCoordinatesByGeomType = geomType => {
+  switch (geomType) {
+    case LINESTRING:
+    case MULTI_POINT:
+      return [[], []];
+    case POLYGON:
+    case MULTI_LINESTRING:
+      return [[[], []]];
+    case MULTI_POLYGON:
+      return [[[[], []]]];
+    case GEOMETRY_COLLECTION:
+      return [{}];
+    case POINT:
+    default:
+      return [];
+  }
+};
+
+export const getJSONSchemaFromGeom = (
+  { identifier, geom, geom_type: geomType, title, ...rest },
+  notRequired,
+) => {
+  const type = geomTypes[geomType];
+  const value = geom || { type, coordinates: [] };
+  const { coordinates } = geom || { coordinates: getDefaultCoordinatesByGeomType(geomType) };
+  return {
+    ...rest,
+    display_value: value,
+    method: (identifier !== null) ? 'PATCH' : 'POST',
+    schema: {
+      properties: {
+        type: {
+          type: 'string',
+          title: 'Type',
+        },
+        coordinates: {
+          items: getSchemaCoordinates(coordinates),
+          type: 'array',
+        },
+      },
+      required: ['type', !notRequired && 'coordinates'].filter(Boolean),
+      title,
+      type: 'object',
+    },
+    ui_schema: {
+      'ui:field': 'geometry',
+      coordinates: getUISchemaCoordinates(coordinates),
+      type: {
+        'ui:widget': 'hidden',
+      },
+    },
+    title,
+    type: 'object',
+    value,
+  };
+};
+
+export default {
+  getJSONSchemaFromGeom,
+  getObjectOrderedValue,
+  isTableObject,
+  sanitizeCustomEndpoint,
+};

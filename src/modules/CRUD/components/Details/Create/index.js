9113387f788b69440ba3baf0135e760de9c0aa44
Florian Sommariva <florian.sommariva@makina-corpus.com>
2020-03-23 11:36:23 +0100

    Refacto create view

---
+++ src/modules/CRUD/components/Details/Create/index.js
@@ -0,0 +1,62 @@
+import { withRouter } from 'react-router-dom';
+import { withTranslation } from 'react-i18next';
+import { connectAuthProvider } from '@terralego/core/modules/Auth';
+
+import { connectAppProvider } from '../../../../../components/AppProvider';
+import { connectCRUDProvider } from '../../../services/CRUDProvider';
+import { withTableFilters } from '../../../services/UserSettingsProvider';
+import { getView, getLayersPaints } from '../../../services/CRUD';
+import compose from '../../../../../utils/compose';
+
+import Create from './Create';
+
+const appProviderGetter = ({
+  env: { modules: { CRUD: { settings } } },
+}) => ({
+  settingsEndpoint: settings,
+});
+
+const authProviderGetter = ({
+  authenticated,
+  user,
+}) => {
+  const permissions = authenticated ? user.permissions : [];
+  return {
+    displayAddFeature: permissions.includes('can_add_feature'),
+    displayChangeFeature: permissions.includes('can_change_feature'),
+  };
+};
+
+const CRUDPRoviderGetter = ({
+  getSettings,
+  getFeaturesList,
+  settings,
+  map,
+  feature,
+  saveFeature,
+  errors: {
+    feature: featureError,
+  },
+}, {
+  match: { params: { layer, id } },
+}) => ({
+  getSettings,
+  getFeaturesList,
+  map,
+  feature: feature[id] || {},
+  saveFeature,
+  view: getView(settings, layer),
+  layerPaint: getLayersPaints(settings).find(item => item['source-layer'] === layer) || {},
+  paramLayer: layer,
+  paramId: id,
+  featureError: featureError.find(({ featureId }) => featureId === id),
+});
+
+export default compose(
+  withRouter,
+  connectAppProvider(appProviderGetter),
+  connectAuthProvider(authProviderGetter),
+  connectCRUDProvider(CRUDPRoviderGetter),
+  withTableFilters(),
+  withTranslation(),
+)(Create);

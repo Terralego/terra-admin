43bfa4a4e3553d3334ba86aa983319cbbbcb12e1
Florian Sommariva <florian.sommariva@makina-corpus.com>
2020-04-23 11:01:57 +0200

    Create DeleteAttachment component

---
+++ src/modules/CRUD/components/Details/Read/AttachmentView/DeleteAttachment/index.js
@@ -0,0 +1,8 @@
+import { withTranslation } from 'react-i18next';
+import compose from '../../../../../../../utils/compose';
+
+import DeleteAttachment from './DeleteAttachment';
+
+export default compose(
+  withTranslation(),
+)(DeleteAttachment);

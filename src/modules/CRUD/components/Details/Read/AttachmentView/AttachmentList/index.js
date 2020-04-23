d212376e56fcbaf5f5e07ae5d2034f8f75ba0985
Florian Sommariva <florian.sommariva@makina-corpus.com>
2020-04-23 11:20:02 +0200

    Add Edit and delete attachment features

--- src/modules/CRUD/components/Details/Read/AttachmentView/AttachmentList/index.js
+++ src/modules/CRUD/components/Details/Read/AttachmentView/AttachmentList/index.js
@@ -1,3 +1,25 @@
+import { withRouter } from 'react-router-dom';
+import { connectCRUDProvider } from '../../../../../services/CRUDProvider';
+import { getView } from '../../../../../services/CRUD';
+import compose from '../../../../../../../utils/compose';
+
 import AttachmentList from './AttachmentList';
 
-export default AttachmentList;
+
+const CRUDPRoviderGetter = ({
+  fetchFeature,
+  settings,
+}, {
+  match: { params: { layer } },
+}) => {
+  const { featureEndpoint } = getView(settings, layer);
+  return {
+    featureEndpoint,
+    fetchFeature,
+  };
+};
+
+export default compose(
+  withRouter,
+  connectCRUDProvider(CRUDPRoviderGetter),
+)(AttachmentList);

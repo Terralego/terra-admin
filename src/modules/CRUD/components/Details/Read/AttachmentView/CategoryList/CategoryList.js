f0b2d92bd833e18ace6277b8e497ed8deb3b9a09
Florian Sommariva <florian.sommariva@makina-corpus.com>
2020-04-24 15:30:57 +0200

    Display a message when there are no attachments yet

--- src/modules/CRUD/components/Details/Read/AttachmentView/CategoryList/CategoryList.js
+++ src/modules/CRUD/components/Details/Read/AttachmentView/CategoryList/CategoryList.js
@@ -1,13 +1,22 @@
 import React, { Fragment, useMemo } from 'react';
 import PropTypes from 'prop-types';
-import { Navbar } from '@blueprintjs/core';
+import { Navbar, Intent } from '@blueprintjs/core';
+import Message from '../../../../../../../components/Message';
 
 import AttachmentList from '../AttachmentList';
 
-const CategoryList = ({ categories, name }) => {
+const CategoryList = ({ categories, name, t }) => {
   const categoryListFilled = useMemo(() =>
     categories.filter(category => category[name].length > 0),
   [categories, name]);
+
+  if (!categoryListFilled.length) {
+    return (
+      <Message intent={Intent.PRIMARY}>
+        {t('CRUD.details.attachment.empty')}
+      </Message>
+    );
+  }
 
   return (
     <>
@@ -28,11 +37,13 @@ const CategoryList = ({ categories, name }) => {
 CategoryList.propTypes = {
   name: PropTypes.oneOf(['attachments', 'pictures']),
   categories: PropTypes.arrayOf(PropTypes.shape({})),
+  t: PropTypes.func,
 };
 
 CategoryList.defaultProps = {
   name: 'attachments',
   categories: [],
+  t: () => {},
 };
 
 export default CategoryList;

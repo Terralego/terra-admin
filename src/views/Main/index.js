8c08a45095a8a1cfed149aaa95aa1f4844fd522c
Florian Sommariva <florian.sommariva@makina-corpus.com>
2020-06-12 10:20:14 +0200

    Set the language driven by api/settings

--- src/views/Main/index.js
+++ src/views/Main/index.js
@@ -1,16 +1,13 @@
 import { connectAuthProvider } from '@terralego/core/modules/Auth';
-import { withTranslation } from 'react-i18next';
 import { connectAppProvider } from '../../components/AppProvider';
 import { withLocale } from '../../components/Locale';
 import Main from './Main';
 
 export default
-withTranslation()(
-  withLocale(
-    connectAppProvider('env', 'errorSettings')(
-      connectAuthProvider('authenticated')(
-        Main,
-      ),
+withLocale(
+  connectAppProvider('env', 'errorSettings')(
+    connectAuthProvider('authenticated')(
+      Main,
     ),
   ),
 );

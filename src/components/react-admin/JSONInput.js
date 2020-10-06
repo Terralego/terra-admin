1bfd1d9fefa98df3a764e824a92e2550db906bab
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2020-10-06 11:42:34 +0200

    Fix JSONInput default value

--- src/components/react-admin/JSONInput.js
+++ src/components/react-admin/JSONInput.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {
   Labeled,
-  addField,
+  useInput,
 } from 'react-admin';
 
 import { JsonEditor as Editor } from 'jsoneditor-react';
@@ -12,42 +12,39 @@ import 'brace/mode/json';
 import 'brace/theme/github';
 
 
-const sanitizeRestProps = ({
-  basePath, i18n, i18nOptions, id, index,
-  lng, resource, source, tReady, ...rest
-}) => rest;
-
 const sanitizeValue = (value, defaultValue) => (
   typeof value === 'object'
     ? value
     : defaultValue
 );
 
-export const JSONInput = addField(
-  ({
-    meta,
-    source,
-    defaultValue = {},
-    input: { value, onChange },
-    ...props
-  }) => (
+const allowedModes = ['code', 'tree'];
+
+export const JSONInput = props => {
+  const {
+    input: { value, ...rest },
+    meta: { error },
+  } = useInput(props);
+
+  const { source, initialValue = {} } = props;
+
+  return (
     <Labeled label={source} {...props}>
       <>
         <Editor
-          value={sanitizeValue(value, defaultValue)}
+          value={sanitizeValue(value, initialValue)}
           ace={ace}
           theme="ace/theme/github"
           mode="code"
-          allowedModes={['code', 'tree']}
+          allowedModes={allowedModes}
           navigationBar={false}
           search={false}
-          onChange={onChange}
-          {...sanitizeRestProps(props)}
+          {...rest}
         />
-        {meta.error && <p className="error">{meta.error}</p>}
+        {error && <p className="error">{error}</p>}
       </>
     </Labeled>
-  ),
-);
+  );
+};
 
 export default JSONInput;

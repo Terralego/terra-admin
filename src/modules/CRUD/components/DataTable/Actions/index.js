90248b064681348357d3395c703375c40afaf90d
Florian Sommariva <florian.sommariva@makina-corpus.com>
2020-11-27 15:58:22 +0100

    Optimize DataTable/Actions component

--- src/modules/CRUD/components/DataTable/Actions/index.js
+++ src/modules/CRUD/components/DataTable/Actions/index.js
@@ -1,10 +1,3 @@
-import { withTranslation } from 'react-i18next';
-import { withTableSize } from '../../../services/UserSettingsProvider';
-import compose from '../../../../../utils/compose';
-
 import Actions from './Actions';
 
-export default compose(
-  withTranslation(),
-  withTableSize(),
-)(Actions);
+export default Actions;

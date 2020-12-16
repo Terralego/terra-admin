111003b314f3a3f53dc5e9741cfd94171bf42491
J-E Castagnede <j.e.castagnede@gmail.com>
2020-12-16 14:53:41 +0100

    fix read modules in user settings

--- src/hoc/withUserSettings.js
+++ src/hoc/withUserSettings.js
@@ -1,11 +1,10 @@
 import { connectAuthProvider } from '@terralego/core/modules/Auth';
-import { connectAppProvider } from '../components/AppProvider';
 
 const permissionsGetter = ({ user }) => (user ? { permissions: user.permissions } : {});
 export const withPermissions = connectAuthProvider(permissionsGetter);
 
 const enabledModulesGetter = ({ user }) => (user ? { enabled_modules: user.modules } : {});
-export const withEnabledModules = connectAppProvider(enabledModulesGetter);
+export const withEnabledModules = connectAuthProvider(enabledModulesGetter);
 
 export default {
   withEnabledModules,

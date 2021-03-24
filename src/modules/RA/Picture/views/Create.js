5a0de052b3934fab9d1320ce365cf86d254a763a
vllorca <vivien.llorca@makina-corpus.com>
2021-03-24 16:30:03 +0100

    Add redirection based on the previous page

--- src/modules/RA/Picture/views/Create.js
+++ src/modules/RA/Picture/views/Create.js
@@ -4,14 +4,19 @@ import { Create } from 'react-admin';
 import PictureFields from '../components/PictureFields';
 import DefaultActions from '../../../../components/react-admin/DefaultActions';
 
-export const PictureCreate = props => (
-  <Create
-    {...props}
-    actions={<DefaultActions redirect="list" />}
-  >
-    <PictureFields redirect="list" />
-  </Create>
-);
+
+export const PictureCreate = props => {
+  const { location: { state: { redirect = 'list' } = {} } } = props;
+
+  return (
+    <Create
+      {...props}
+      actions={<DefaultActions redirect={redirect} />}
+    >
+      <PictureFields redirect={redirect} />
+    </Create>
+  );
+};
 
 
 export default PictureCreate;

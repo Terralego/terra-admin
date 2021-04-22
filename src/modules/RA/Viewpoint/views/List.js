abb84dca190208cf68141a6e53d4031208c041ae
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2021-04-22 15:39:02 +0200

    Add ordering to OPP views

--- src/modules/RA/Viewpoint/views/List.js
+++ src/modules/RA/Viewpoint/views/List.js
@@ -1,9 +1,34 @@
-import React from 'react';
-import { List } from 'react-admin';
+import React, { cloneElement } from 'react';
+import { List, SortButton, TopToolbar, CreateButton, useListContext } from 'react-admin';
 
 import CommonBulkActionButtons from '../../../../components/react-admin/CommonBulkActionButtons';
 import GridList from '../../../../components/react-admin/GridList';
 import ListFilters from './ListFilters';
+
+const ListActions = ({
+  filters,
+}) => {
+  const {
+    resource,
+    displayedFilters,
+    filterValues,
+    basePath,
+    showFilter,
+  } = useListContext();
+  return (
+    <TopToolbar>
+      {filters && cloneElement(filters, {
+        resource,
+        showFilter,
+        displayedFilters,
+        filterValues,
+        context: 'button',
+      })}
+      <SortButton fields={['label', 'id']} />
+      <CreateButton basePath={basePath} />
+    </TopToolbar>
+  );
+};
 
 
 export const ViewpointList = props => (
@@ -11,6 +36,7 @@ export const ViewpointList = props => (
     {...props}
     exporter={false}
     filters={<ListFilters />}
+    actions={<ListActions />}
     bulkActionButtons={<CommonBulkActionButtons />}
   >
     <GridList />

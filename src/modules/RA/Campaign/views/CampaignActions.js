426e8b1e3139955316f3beab2fc4f5bc6b902400
Cédric Farcy <cedric.farcy@makina-corpus.com>
2021-05-05 15:06:58 +0200

    Add button to notify admin that campaign has been updated

--- src/modules/RA/Campaign/views/CampaignActions.js
+++ src/modules/RA/Campaign/views/CampaignActions.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TopToolbar, Button, useTranslate } from 'react-admin';
+import { TopToolbar, Button, useTranslate, useDataProvider } from 'react-admin';
 
 import { Link } from 'react-router-dom';
 import IconArrowBack from '@material-ui/icons/ArrowBack'; // eslint-disable-line import/no-extraneous-dependencies
@@ -7,17 +7,39 @@ import IconList from '@material-ui/icons/List'; // eslint-disable-line import/no
 import FileCopy from '@material-ui/icons/FileCopy'; // eslint-disable-line import/no-extraneous-dependencies
 import Api from '@terralego/core/modules/Api';
 
+import { toast } from '../../../../utils/toast';
+
 import CloneCampaignButton from '../components/CloneCampaignButton';
 
 
 const CampaignActions = ({
   basePath,
   redirect,
-  data,
+  data = {},
 }) => {
   const translate = useTranslate();
+  const dataProvider = useDataProvider();
+
+  const notifyAdmin = React.useCallback(async () => {
+    try {
+      const { data: d = {} } = await dataProvider.getOne('campaign', { id: `${data.id}/notify_admin` });
+
+      // TODO: inform user that notification has been sent to the admin
+      toast.displayToaster(d, `Admin has been notify that campaign ${d.label} has been updated.`);
+    } catch (err) {
+      toast.displayError(err.message);
+    }
+  }, [data.id, dataProvider]);
+
   return (
     <TopToolbar>
+      {(data && data.id) && (
+        <Button
+          variant="outlined"
+          label="campaign.notify-admin"
+          onClick={notifyAdmin}
+        />
+      )}
       {(data && data.id) && (
       <CloneCampaignButton
         record={data}

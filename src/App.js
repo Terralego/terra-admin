52c8b705bb1374e982927e947dff89ff8b69ae5d
Florian Sommariva <florian.sommariva@makina-corpus.com>
2021-05-18 15:20:40 +0200

    Remove uncessary blueprint package

--- src/App.js
+++ src/App.js
@@ -7,10 +7,9 @@ import './config/i18n';
 import AppProvider from './components/AppProvider';
 import Main from './views/Main';
 
-import '@blueprintjs/core/lib/css/blueprint.css';
-import '@blueprintjs/datetime/lib/css/blueprint-datetime.css';
-import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 import 'normalize.css';
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
+import '@blueprintjs/core/lib/css/blueprint.css';
 import { BetaProvider } from './hooks/useBeta';
 
 const { PUBLIC_URL } = process.env;

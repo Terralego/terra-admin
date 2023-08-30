409714199a935bc60f0d7f485a1d448da1100255
Cédric Farcy <cedric.farcy@makina-corpus.com>
2023-08-30 14:51:01 +0200

    Add notification after triggering refresh

--- src/modules/RA/DataSource/components/DataSourceEditActions.js
+++ src/modules/RA/DataSource/components/DataSourceEditActions.js
@@ -3,6 +3,7 @@ import {
   TopToolbar,
   ListButton,
   RefreshButton,
+  useNotify,
   withDataProvider,
 } from 'react-admin';
 
@@ -20,27 +21,47 @@ const style = () => ({
 
 const DataSourceEditActions = ({
   dataProvider,
-  data: { id, report } = {},
+  data: { id, report, _type } = {},
   basePath,
   classes,
-}) => (
-  <TopToolbar>
-    <ListButton
-      basePath={basePath}
-      variant="outlined"
-      label="ra.action.back-to-list"
-      className={classes.list}
-    />
-    {/* 'report' is null when a source has just been created */}
-    <StatusChip label="datasource.form.status" status={report ?? {}} />
-    <RefreshButton
-      color="primary"
-      variant="contained"
-      label="datasource.edit.refresh"
-      onClick={() => dataProvider('REFRESH', RES_DATASOURCE, { id })}
-    />
-  </TopToolbar>
-);
+}) => {
+  const [pending, setPending] = React.useState(null);
+  const notify = useNotify();
+
+  const refreshData = React.useCallback(async () => {
+    try {
+      setPending(true);
+      await dataProvider('REFRESH', RES_DATASOURCE, { id });
+      notify('datasource.form.refresh.accepted', { type: 'success' });
+    } catch {
+      setPending(false);
+      notify('datasource.form.refresh.notAllowed', { type: 'error' });
+    }
+  }, [setPending, dataProvider, notify, id]);
+
+  return (
+    <TopToolbar>
+      <ListButton
+        basePath={basePath}
+        variant="outlined"
+        label="ra.action.back-to-list"
+        className={classes.list}
+      />
+      {/* 'report' is null when a source has just been created */}
+      <StatusChip label="datasource.form.status" status={report ?? {}} />
+      { _type !== 'WMTSSource' && (
+        <RefreshButton
+          color="primary"
+          variant="contained"
+          label="datasource.edit.refresh"
+          onClick={refreshData}
+          disabled={pending}
+        />
+      )}
+    </TopToolbar>
+  );
+};
+
 
 export default compose(
   withStyles(style),

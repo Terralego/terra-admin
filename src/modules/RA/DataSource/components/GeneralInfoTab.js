87942d3ccf2d53ad144104c61b5e25589456c23d
Cédric Farcy` <cfarcy.dev@gmail.com>
2023-12-15 10:23:19 +0100

    Add layerlist to datasource general infos

--- src/modules/RA/DataSource/components/GeneralInfoTab.js
+++ src/modules/RA/DataSource/components/GeneralInfoTab.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import { List, ListItem, ListItemText } from '@material-ui/core';
-import { ReferenceField, FunctionField } from 'react-admin';
-import { RES_USER } from '../../ra-modules';
+import { List, ListItem, ListItemText, makeStyles } from '@material-ui/core';
+import {
+  ReferenceField,
+  FunctionField,
+  ReferenceArrayField,
+  SingleFieldList,
+  ChipField,
+} from 'react-admin';
+import { RES_USER, RES_DATALAYER } from '../../ra-modules';
 
+const useStyle = makeStyles({
+  layers: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+  },
+});
 
 const GeneralInfoTab = ({
   translate,
@@ -13,53 +26,64 @@ const GeneralInfoTab = ({
     credit,
     author,
   },
-}) => (
-  <List>
-    <ListItem>
-      <ListItemText
-        primary={translate('datasource.form.infos.created')}
-        secondary={new Date(createdAt).toLocaleString()}
-      />
-    </ListItem>
-    <ListItem>
-      <ListItemText
-        primary={translate('datasource.form.infos.updated')}
-        secondary={new Date(updatedAt).toLocaleString()}
-      />
-    </ListItem>
-    <ListItem>
-      <ListItemText
-        primary={translate('datasource.form.infos.description')}
-        secondary={description || translate('datasource.form.infos.no-description')}
-      />
-    </ListItem>
-    <ListItem>
-      <ListItemText
-        primary={translate('datasource.form.infos.credit')}
-        secondary={credit || translate('datasource.form.infos.no-credit')}
-      />
-    </ListItem>
-    <ListItem>
-      {author
-        ? (
-          <ReferenceField source="author" reference={RES_USER}>
-            <FunctionField
-              render={({ email }) => (
-                <ListItemText
-                  primary={translate('datasource.form.infos.author')}
-                  secondary={email}
-                />
-              )}
+}) => {
+  const classes = useStyle();
+  return (
+    <List>
+      <ListItem>
+        <ListItemText
+          primary={translate('datasource.form.infos.created')}
+          secondary={new Date(createdAt).toLocaleString()}
+        />
+      </ListItem>
+      <ListItem>
+        <ListItemText
+          primary={translate('datasource.form.infos.updated')}
+          secondary={new Date(updatedAt).toLocaleString()}
+        />
+      </ListItem>
+      <ListItem>
+        <ListItemText
+          primary={translate('datasource.form.infos.description')}
+          secondary={description || translate('datasource.form.infos.no-description')}
+        />
+      </ListItem>
+      <ListItem>
+        <ListItemText
+          primary={translate('datasource.form.infos.credit')}
+          secondary={credit || translate('datasource.form.infos.no-credit')}
+        />
+      </ListItem>
+      <ListItem>
+        {author
+          ? (
+            <ReferenceField source="author" reference={RES_USER}>
+              <FunctionField
+                render={({ email }) => (
+                  <ListItemText
+                    primary={translate('datasource.form.infos.author')}
+                    secondary={email}
+                  />
+                )}
+              />
+            </ReferenceField>
+          ) : (
+            <ListItemText
+              primary={translate('datasource.form.infos.author')}
+              secondary={translate('datasource.form.infos.no-author')}
             />
-          </ReferenceField>
-        ) : (
-          <ListItemText
-            primary={translate('datasource.form.infos.author')}
-            secondary={translate('datasource.form.infos.no-author')}
-          />
-        )}
-    </ListItem>
-  </List>
-);
+          )}
+      </ListItem>
+      <ListItem className={classes.layers}>
+        <ListItemText primary={translate('datasource.form.layers')} />
+        <ReferenceArrayField source="layers" reference={RES_DATALAYER}>
+          <SingleFieldList>
+            <ChipField source="name" />
+          </SingleFieldList>
+        </ReferenceArrayField>
+      </ListItem>
+    </List>
+  );
+};
 
 export default GeneralInfoTab;
